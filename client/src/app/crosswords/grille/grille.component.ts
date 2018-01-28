8e13ef7226a49c6ecc968c804e4996063e61c366
Adrimeov <jffontaine_@hotmail.com>
2018-01-28 12:43:21 -0500

    Creation du service de requete de grille. Injection et import de dependencies faites. 45 lignes modifie.

---
+++ client/src/app/crosswords/grille/grille.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+import {mockWord} from "../mockObject/mockWord";
+@Component({
+  selector: "app-grille",
+  templateUrl: "./grille.component.html",
+  styleUrls: ["./grille.component.css"]
+})
+export class GrilleComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
---
+++ client/src/app/crosswords/mockObject/mockWord.ts
@@ -0,0 +1,9 @@
+
+export class Mockword {
+public mots: string;
+public definition: string;,
+public vertical: boolean;
+public longeur: number;
+public premierX: number;
+public premierY: number;
+}

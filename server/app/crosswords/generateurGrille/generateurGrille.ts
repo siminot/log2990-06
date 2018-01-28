973da622e326e6ef38fc08dd0b1b71db06d3a0d4
Unknown <fersamw@gmail.com>
2018-01-28 17:21:37 -0500

    SF & WL: Tests cases noires codes

--- server/app/crosswords/generateurGrille/generateurGrille.ts
+++ server/app/crosswords/generateurGrille/generateurGrille.ts
@@ -12,18 +12,20 @@ module Route {
 
         constructor() {
 
-            this.grille = new Array(TAILLE_TEST);
+            this.grille = new Array(TAILLE_TEST).fill("0");
             for (let i: number = 0; i < TAILLE_TEST; i++) {
-                this.grille[i] = new Array(TAILLE_TEST);
+                this.grille[i] = new Array(TAILLE_TEST).fill("0");
             }
 
         }
 
         private genererCasesNoires(ratioVoulu: number): number {
             
+            if (ratioVoulu < 0 || ratioVoulu > 1)
+            return null;
+
             let nombreCases: number = Math.ceil(TAILLE_TEST * TAILLE_TEST * ratioVoulu);
             let compteurCasesNoires: number = 0;
-
             let x: number = 0;
             let y: number = 0;
 
@@ -35,13 +37,25 @@ module Route {
                 if (Math.random() <= ratioVoulu) {
 
                     //Verif si position est valide pour insertion
+                    if (this.verifInsertCaseNoire(x, y))
                     this.grille[y][x] = "-1";
                     compteurCasesNoires++;
                 }
+            }
+            return nombreCases;
+        }
 
+        private verifInsertCaseNoire(positionX: number, positionY: number): Boolean {
+
+             //parcours colonne
+            for (let i: number = 0; i < TAILLE_TEST; i++) {
             }
 
-            return nombreCases;
+            //Parcours rangee
+            for (let i: number = 0; i < TAILLE_TEST; i++) {
+            }
+
+            return true;
         }
 
         public generateurGrille(req: Request, res: Response, next: NextFunction): void {

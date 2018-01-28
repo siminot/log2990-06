6c661ec1e9275b8b6aadb71246d48dd4785b82c5
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-01-28 16:21:57 -0500

    ML: change ServiceLexical en module

--- server/app/crosswords/serviceLexical/ServiceLexical.ts
+++ server/app/crosswords/serviceLexical/ServiceLexical.ts
@@ -1,30 +1,42 @@
 import { Mot } from './Mot';
+import { injectable, } from "inversify";
+import { Request, Response, NextFunction } from "express";
+import { Message } from "../../../../common/communication/message";
+
 //import * as WebRequest from 'web-request';
 
-class ServiceLexical{
+module ServiceLexical{
 
-    public constructor() {
+    @injectable()
+    export class ServiceLexical{
 
-    }
+        public constructor() {}
 
-
-    public async obtenirMotsSelonContrainte(contrainte : string){
-        //let url = "https://api.datamuse.com/words?sp=t??k";
-        //let data = await WebRequest.json<any>(url);
-        this.retirerMotsSansDefinition(null);
-    }
-
-    private retirerMotsSansDefinition(mots : Array<Mot>) : void {
-
-    }
-
-    public obtenirDefinitionsMot(mot : string) : Mot {
-
-
-        if("mot inexsistant")
-            throw Error("Mot inexistant ou sans definition");
-
-        return null;
+        public helloWorld(req: Request, res: Response, next: NextFunction): void {
+            const message: Message = new Message();
+            message.title = "Service";
+            message.body = "Lexical";
+            res.send(JSON.stringify(message));
+        }    
+    
+        public async obtenirMotsSelonContrainte(contrainte : string){
+            //let url = "https://api.datamuse.com/words?sp=t??k";
+            //let data = await WebRequest.json<any>(url);
+            this.retirerMotsSansDefinition(null);
+        }
+    
+        private retirerMotsSansDefinition(mots : Array<Mot>) : void {
+    
+        }
+    
+        public obtenirDefinitionsMot(mot : string) : Mot {
+    
+    
+            if("mot inexsistant")
+                throw Error("Mot inexistant ou sans definition");
+    
+            return null;
+        }
     }
 
 }

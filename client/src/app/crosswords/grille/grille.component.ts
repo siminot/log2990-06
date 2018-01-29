fccfcdec1b6e525fdaf8931ac8533b1f93c86591
Sebastien Cadorette <cado.94@me.com>
2018-01-29 18:19:58 -0500

    SC : Sorry, mon petit cote perfectionniste devait arranger les espaces dans les fichiers du dossier grille ...

--- client/src/app/crosswords/grille/grille.component.ts
+++ client/src/app/crosswords/grille/grille.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 //import {Mockword} from "../mockObject/mockWord";
-//import{objetTest} from "../mockObject/mockWord";
 import {TAILLE_TABLEAU} from "../constantes";
 import { objetTest } from "../mockObject/mockWord";
+
+
 @Component({
   selector: "app-grille",
   templateUrl: "./grille.component.html",
   styleUrls: ["./grille.component.css"]
 })
+
+
 export class GrilleComponent implements OnInit {
   
   genererGrille(): any[][]{
@@ -16,21 +19,18 @@ export class GrilleComponent implements OnInit {
     for(let i:number = 0; i < TAILLE_TABLEAU; i++){
       let row: Array<objetTest> = new Array(TAILLE_TABLEAU);
       for(let j:number = 0; j < TAILLE_TABLEAU; j++){
-       let kalise: objetTest = {case:true, mot:"XXX"};
-       row[j] = kalise; 
+        let kalise: objetTest = {case:true, mot:"XXX"};
+        row[j] = kalise; 
+      }
+      matrice[i] = row; 
     }
-    matrice[i] = row; 
+  
+    return matrice; 
   }
-  
-  return matrice; 
-}
 
-tabarnak: Array<Array<objetTest>> = this.genererGrille();
+  tabarnak: Array<Array<objetTest>> = this.genererGrille();
  
- public constructor() { }
+  public constructor() { }
 
- ngOnInit() {}
-
-  
-
+  ngOnInit() { }
 }

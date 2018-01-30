7e0b897165ae894995cc6defc56eb5a8a56e199c
Sebastien Cadorette <cado.94@me.com>
2018-01-30 11:35:53 -0500

    SC : Ajout d'une fonction pour aller chercher la liste de mots avec un observable.

--- client/src/app/crosswords/service-Requete-de-Grille/requete-de-grille.service.ts
+++ client/src/app/crosswords/service-Requete-de-Grille/requete-de-grille.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@angular/core";
+import { of } from 'rxjs/observable/of';
+
 import { listeMots } from "../mockObject/mockListWord";
 import { Mockword } from "../mockObject/mockWord";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class RequeteDeGrilleService {
 
   public constructor() { }
 
-  public getMots(): Mockword[] {
-    return listeMots;
+  public getMots(): Observable<Mockword[]> {
+    return of(listeMots);
   }
-
-
-
 }

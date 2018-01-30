b76440d1f50cd3662205b259cba2581a9c5b2817
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-01-30 14:18:02 -0500

    ML: requete a l'api externe fonctionne

--- server/app/crosswords/serviceLexical/ServiceLexical.ts
+++ server/app/crosswords/serviceLexical/ServiceLexical.ts
@@ -1,11 +1,14 @@
 import { Mot } from './Mot';
 import { injectable, } from "inversify";
 import { Request, Response, NextFunction } from "express";
-import { Message } from "../../../../common/communication/message";
+//import { Message } from "../../../../common/communication/message";
 
-//import * as WebRequest from 'web-request';
+import * as WebRequest from 'web-request';
 
 module ServiceLexical{
+
+    const URL = "https://api.datamuse.com/words?sp=";
+    const FLAG = "&md=df";
 
     @injectable()
     export class ServiceLexical{
@@ -13,28 +16,30 @@ module ServiceLexical{
         public constructor() {}
 
         public messageServiceLexical(req: Request, res: Response, next: NextFunction): void {
-            const message: Message = new Message();
-            message.title = "Service";
-            message.body = "Lexical";
-            res.send(JSON.stringify(message));
+            //const message: Message = new Message();
+            //message.title = "Service: ";
+            this.obtenirMotsSelonContrainte("f???").then(reponse => {
+                res.send(reponse);
+            });
         }    
     
-        public obtenirMotsSelonContrainte(contrainte : string) : Array<Mot> {
-            let url = "https://api.datamuse.com/words?sp=";
-            url += contrainte + "&md=df";
-            let data;//= WebRequest.json<any>(url);
-            return this.importerJSON(data);
+        public obtenirMotsSelonContrainte(contrainte : string) : Promise<Mot[]> {
+            let url = URL + contrainte + FLAG;
+            let promesse = WebRequest.json<any>(url);
+            let mots: Mot[];
+            return promesse.then((data) => this.importerJSON(data));
+            //return this.importerJSON(data);
         }
     
-        private importerJSON(data : any) : Array<Mot> {
-            let dictionnaire : Array<Mot>;
+        private importerJSON(data : any) : Mot[] {
+            let dictionnaire : Mot[] = [];
             
             //Parcourir le JSON et ajouter les mots
-            let donnees = JSON.parse(data);
-            for(let objet of donnees){
+            // let donnees = JSON.parse(data);
+            for(let objet of data){
                 let mot = new Mot(objet.word, objet.defs, objet.tags[0])
                 if(mot.possedeDefinition())
-                    dictionnaire.fill(mot);
+                    dictionnaire.push(mot);
             }
             
             return dictionnaire;
--- server/app/routes.ts
+++ server/app/routes.ts
@@ -2,13 +2,12 @@ import { injectable, inject } from "inversify";
 import { Router, Request, Response, NextFunction } from "express";
 
 import Types from "./types";
-//import { Index } from "./routes/index";
 import { ServiceLexical } from "./crosswords/serviceLexical/ServiceLexical";
 
 @injectable()
 export class Routes {
 
-    public constructor(@inject(Types.ServiceLexical) /*private index: Index, */private serviceLexical: ServiceLexical) {}
+    public constructor(@inject(Types.ServiceLexical) private serviceLexical: ServiceLexical) {}
 
     public get routes(): Router {
         const router: Router = Router();

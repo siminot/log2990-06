b4ad682d2bba0626761789edc41bbcde8ae7e74f
Unknown <fersamw@gmail.com>
2018-01-30 11:40:46 -0500

    SF & WL: Cherry pick d'un objet "Mot"

---
+++ common/mockObject/mockWord.ts
@@ -0,0 +1,16 @@
+
+export class Mockword {
+    public mots: string;
+    public definition: string;
+    public vertical: boolean;
+    public longeur: number;
+    public premierX: number;
+    public premierY: number;
+    public activer: boolean;
+
+    constructor() {
+
+        
+    }
+
+}

d8a0f29991e3edda6c1ad8b2a222d9a590c36451
Sebastien Cadorette <cado.94@me.com>
2018-01-30 12:15:03 -0500

    SC : Modifi mineur a la syntaxe dans le fichier contenant les mots pour tester notre presentation de la vue de la grille.

--- client/src/app/crosswords/mockObject/mockListWord.ts
+++ client/src/app/crosswords/mockObject/mockListWord.ts
@@ -5,4 +5,4 @@ export const listeMots: Mockword[] = [
     {mots: "Pome", definition: "provient du pommier", vertical: true, longeur: 4, premierX: 0, premierY: 0, activer: false },
     {mots: "Eppa", definition: "Pas super super", vertical: false, longeur: 4, premierX: 0, premierY: 3, activer: false },
     {mots: "Tata", definition: "ben nice is ben cool", vertical: true, longeur: 4, premierX: 3, premierY: 0, activer: false }
-]
+];

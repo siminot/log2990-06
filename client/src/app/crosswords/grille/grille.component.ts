a40038684d9422712ee9b633c5aae3d2feeb48a6
Sebastien Cadorette <cado.94@me.com>
2018-02-02 10:23:10 -0500

    SC : On met ca beau avec TSLint.

--- client/src/app/crosswords/grille/grille.component.ts
+++ client/src/app/crosswords/grille/grille.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { Mockword } from "../mockObject/mockWord";
+import { Mockword, objetTest } from "../mockObject/mockWord";
 import { TAILLE_TABLEAU } from "../constantes";
-import { objetTest } from "../mockObject/mockWord";
-import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-grille.service';
-
+import { RequeteDeGrilleService } from "../service-Requete-de-Grille/requete-de-grille.service";
 
 @Component({
   selector: "app-grille",
@@ -11,14 +9,12 @@ import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-
   styleUrls: ["./grille.component.css"]
 })
 
-
 export class GrilleComponent implements OnInit {
-  mots:Mockword[];
-  motsAAfiicher:String[];
-  matriceVide: Array<Array<objetTest>>;
-  dessu:boolean; 
-  compteur:number;
-
+  private mots: Mockword[];
+  private motsAAfficher: String[];
+  private matriceVide: Array<Array<objetTest>>;
+  private dessu: boolean;
+  private compteur: number;
 
   public constructor(private listeMotsService: RequeteDeGrilleService) {
     this.matriceVide = this.genererGrille();
@@ -26,34 +22,31 @@ export class GrilleComponent implements OnInit {
     this.dessu = false;
   }
 
-
-  ngOnInit() { 
+  ngOnInit() {
     this.getMots();
     this.putWordsInGrid();
   }
 
-
-  genererGrille(): Array<Array<objetTest>>{
+  private genererGrille(): Array<Array<objetTest>> {
     let matrice: Array<Array<objetTest>> = new Array(TAILLE_TABLEAU);
 
-    for(let i:number = 0; i < TAILLE_TABLEAU; i++){
+    for (let i: number = 0; i < TAILLE_TABLEAU; i++){
       let row: Array<objetTest> = new Array(TAILLE_TABLEAU);
-      for(let j:number = 0; j < TAILLE_TABLEAU; j++){
-        let caseNoir:objetTest;
+      for (let j: number = 0; j < TAILLE_TABLEAU; j++){
+        let caseNoir: objetTest;
 
         if(j%2==0)
           caseNoir = {case:true, mot:"X"};
         else
           caseNoir = {case:true, mot:"P"};
         
-        row[j] = caseNoir; 
+        row[j] = caseNoir;
       }
-      matrice[i] = row; 
+      matrice[i] = row;
     }
   
-    return matrice; 
+    return matrice;
   }
-
 
   myStyle(etat:boolean): string{
     if(!etat)
@@ -61,11 +54,10 @@ export class GrilleComponent implements OnInit {
     return("1")
   }
 
-  
+
   getMots(): void {
     this.listeMotsService.getMots().subscribe(mots => this.mots = mots);
   }
-
 
   putWordsInGrid(): void {
     for(let mot of this.mots) {

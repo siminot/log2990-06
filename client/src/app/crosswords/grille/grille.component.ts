82214c7b040c6fe3c4312700de5633a48d30e000
Sebastien Cadorette <cado.94@me.com>
2018-02-02 18:30:39 -0500

    SC : Problème de la grille fixé. Chaque case peut contenir une lettre indépendante.

--- client/src/app/crosswords/grille/grille.component.ts
+++ client/src/app/crosswords/grille/grille.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Word } from "../mockObject/word";
-import { TAILLE_TABLEAU , caseNoir } from "../constantes";
+import { TAILLE_TABLEAU } from "../constantes";
 import { lettreGrille } from "../mockObject/word";
 import { RequeteDeGrilleService } from "../service-Requete-de-Grille/requete-de-grille.service";
 
 @Component({
   selector: "app-grille",
   templateUrl: "./grille.component.html",
-  styleUrls: ["./grille.component.css"],
+  styleUrls: ["./grille.component.css"]
 })
 
 export class GrilleComponent implements OnInit {
@@ -36,6 +36,7 @@ export class GrilleComponent implements OnInit {
     for(let i:number = 0; i < TAILLE_TABLEAU; i++){
       let row: Array<lettreGrille> = new Array(TAILLE_TABLEAU);
       for(let j:number = 0; j < TAILLE_TABLEAU; j++) {
+        let caseNoir: lettreGrille = {case:true, mot:"1", lettre:false};
         row[j] = caseNoir;
       }
       matrice[i] = row;
@@ -58,6 +59,7 @@ export class GrilleComponent implements OnInit {
     // this.matriceVide[this.mots[0].premierX][this.mots[0].premierY].lettre = true;
       
     // console.log(this.matriceVide);
+    let tmp: lettreGrille = {case:true,mot:"A",lettre:true};
     this.matriceVide[0][0].mot = "A";
   }
 }

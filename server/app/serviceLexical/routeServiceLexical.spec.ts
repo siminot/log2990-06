861d06fd377878659150d49f0374ce74104b8920
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-02-03 17:47:14 -0500

    ML: début des tests routeServiceLexical

--- server/app/serviceLexical/routeServiceLexical.spec.ts
+++ server/app/serviceLexical/routeServiceLexical.spec.ts
@@ -1,36 +1,34 @@
 {
-    // const assert = require("assert");
-    const difficulte = require("./Difficulte");
-    const contrainteMot = require("./ContrainteMot");
-    const expect = require("expect");
+    const assert = require("assert");
+    // const expect = require("expect");
 
     describe("routeServiceLexical", () => {
         describe("Constructeur", () => {
 
-            it("doit retourner une erreur (longueur mot negative)", function() {
-                expect(contrainteMot.constructor(-5, difficulte.Facile)).toThrowError;
+            it("doit creer un service web", () => {
+                assert(true);
             });
 
-            it("doit etre construit", function() {
+            /*it("doit etre construit", function() {
                 expect(contrainteMot.constructor(4, difficulte.Moyen)).toBeDefined();
-            });
+            });*/
 
         });
 
-        describe("ajouterContrainte", () => {
-            const contrainte = contrainteMot.constructor(5, difficulte.Facile);
+        describe("accederRoute", () => {
+            // const contrainte = contrainteMot.constructor(5, difficulte.Facile);
 
-            it("doit retourner une erreur (indice negatif)", function() {
-                expect(contrainte.ajouterContrainte(-5, "A")).toThrowError;
+            it("doit retourner un tableau de mots", () => {
+                assert(true);
             });
 
-            it("doit retourner une erreur (pas une lettre)", function() {
+            /*it("doit retourner une erreur (pas une lettre)", function() {
                 expect(contrainte.ajouterContrainte(5, "-")).toThrowError;
             });
 
             it("ne doit pas retourner d'erreur", function() {
                 expect(contrainte.ajouterContrainte(5, "A")).not.toThrowError();
-            });
+            });*/
 
         });
     });

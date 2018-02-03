c10b4744e5d4ee34032c964c35c4ff3c650d8138
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-02-03 18:40:27 -0500

    ML: test routeServiceLexical

--- server/app/serviceLexical/routeServiceLexical.spec.ts
+++ server/app/serviceLexical/routeServiceLexical.spec.ts
@@ -1,17 +1,23 @@
+// import { RouteServiceLexical } from "./routeServiceLexical";
+// import { ServiceLexical } from "./ServiceLexical";
+
 {
     const assert = require("assert");
-    // const expect = require("expect");
+    const expect = require("expect");
+    const routeServiceLexical = require("routeServiceLexical");
+    const serviceLexical = require("serviceLexical");
 
     describe("routeServiceLexical", () => {
         describe("Constructeur", () => {
 
             it("doit creer un service web", () => {
-                assert(true);
+                // const service: ServiceLexical = new ServiceLexical();
+                // const route: RouteServiceLexical = new RouteServiceLexical(service);
+                expect(routeServiceLexical).to.be.an("object");
             });
         });
 
         describe("accederRoute", () => {
-            // const contrainte = contrainteMot.constructor(5, difficulte.Facile);
 
             it("doit retourner un tableau de mots", () => {
                 assert(true);

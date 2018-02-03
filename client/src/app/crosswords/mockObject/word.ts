0e7b0cd730ce4094136bb7f195e1d827d7de73c9
Sebastien Cadorette <cado.94@me.com>
2018-02-03 11:08:57 -0500

    SC : Identation dans le fichier word.

--- client/src/app/crosswords/mockObject/word.ts
+++ client/src/app/crosswords/mockObject/word.ts
@@ -1,16 +1,16 @@
 
 export class Word {
-public lettres: string;
-public definition: string;
-public vertical: boolean;
-public longeur: number;
-public premierX: number;
-public premierY: number;
-public activer: boolean;
+    public lettres: string;
+    public definition: string;
+    public vertical: boolean;
+    public longeur: number;
+    public premierX: number;
+    public premierY: number;
+    public activer: boolean;
 }
 
 export class lettreGrille {
-public case: boolean; 
-public lettre:string; 
-public caseDecouverte:boolean;
+    public case: boolean; 
+    public lettre:string; 
+    public caseDecouverte:boolean;
 }

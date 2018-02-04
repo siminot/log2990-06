91124d79ced0b2d1aa66bf4a428a9e58a2a53ae9
Sebastien Cadorette <cado.94@me.com>
2018-02-04 15:59:26 -0500

    SC : Struggle avec les tests. How does it work.

--- client/src/app/crosswords/definition/definition.component.spec.ts
+++ client/src/app/crosswords/definition/definition.component.spec.ts
@@ -1,9 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as assert from 'assert';
 import { DefinitionComponent } from './definition.component';
+import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-grille.service';
+import { listeMots } from '../mockObject/mockListWord';
 
 describe('DefinitionComponent', () => {
-  let component: DefinitionComponent;
+  let service: RequeteDeGrilleService = new RequeteDeGrilleService();
+  let component: DefinitionComponent = new DefinitionComponent(service);
   let fixture: ComponentFixture<DefinitionComponent>;
 
   beforeEach(async(() => {
@@ -19,7 +22,16 @@ describe('DefinitionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Création d\'objets.', () => {  
+    it('objet Definition créé', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('Modification de la grille.', () => {
+    it('Découvrir les cases dans la grille selon le mot selectionné.', () => {
+      
+      assert.ok(true);
+    });
   });
 });

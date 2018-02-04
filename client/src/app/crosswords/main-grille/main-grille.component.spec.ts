3657d08087b988f9f505e2097ce499c612f83d51
Sebastien Cadorette <cado.94@me.com>
2018-02-04 15:59:41 -0500

    SC : Suppression d'un import inutile.

--- client/src/app/crosswords/main-grille/main-grille.component.spec.ts
+++ client/src/app/crosswords/main-grille/main-grille.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainGrilleComponent } from './main-grille.component';
-import { DefinitionComponent } from '../definition/definition.component';
 
 describe('MainGrilleComponent', () => {
   let component: MainGrilleComponent;

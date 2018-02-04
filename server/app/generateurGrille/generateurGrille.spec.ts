0b18564ecbcc9298faaae769e80c5f148cc2c097
Samuel Ferron <fersamw@gmail.com>
2018-02-04 15:16:36 -0500

    SF & WL: Fragmentation de classe

--- server/app/generateurGrille/generateurGrille.spec.ts
+++ server/app/generateurGrille/generateurGrille.spec.ts
@@ -10,30 +10,30 @@ import { Mockword } from "./../../../common/mockObject/mockWord";
 
     describe("tests generation des cases noires", () => {
 
-        it("fait bien 50% cases, soit 8 cases", (done) => {
-            assert.equal(genTest.initCasesNoires(0.5), 8);
-            done();
-        });
-
-        // it('fait bien 100% cases, soit 16 cases', (done) => {
-        //     assert.equal(genTest.initCasesNoires(1), 16);
+        // it("fait bien 50% cases, soit 8 cases", (done) => {
+        //     assert.equal(genTest.initCasesNoires(0.5), 8);
         //     done();
         // });
 
-        it("fait bien 0% cases, soit 0 cases", (done) => {
-            assert.equal(genTest.initCasesNoires(0), 0);
-            done();
-        });
+        // // it('fait bien 100% cases, soit 16 cases', (done) => {
+        // //     assert.equal(genTest.initCasesNoires(1), 16);
+        // //     done();
+        // // });
 
-        it("Doit retourner NULL avec une entree negative", (done) => {
-            assert.equal(genTest.initCasesNoires(-10), null);
-            done();
-        });
+        // it("fait bien 0% cases, soit 0 cases", (done) => {
+        //     assert.equal(genTest.initCasesNoires(0), 0);
+        //     done();
+        // });
 
-        it("Doit retourner NULL avec une entree superieure a 1", (done) => {
-            assert.equal(genTest.initCasesNoires(2), null);
-            done();
-        });
+        // it("Doit retourner NULL avec une entree negative", (done) => {
+        //     assert.equal(genTest.initCasesNoires(-10), null);
+        //     done();
+        // });
+
+        // it("Doit retourner NULL avec une entree superieure a 1", (done) => {
+        //     assert.equal(genTest.initCasesNoires(2), null);
+        //     done();
+        // });
 
   });
 
--- server/app/generateurGrille/generateurGrille.ts
+++ server/app/generateurGrille/generateurGrille.ts
@@ -3,9 +3,11 @@ import "reflect-metadata";
 import { injectable, } from "inversify";
 import * as WebRequest from "web-request";
 
-import { TAILLE_TEST, VIDE, NOIR, POURCENTAGE_TEST } from "./constantes";
+import { TAILLE_TEST, VIDE, NOIR } from "./constantes";
 import { Mockword } from "./../../../common/mockObject/mockWord";
 import { Mot } from "./../../../common/communication/Mot";
+
+import { GenerateurSquelette } from "./generateurSquelette";
 
 module Route {
 
@@ -15,17 +17,15 @@ module Route {
         private grille: Array<Array<string>>;
         private listeMot: Array<Mockword> = new Array<Mockword>();
         private tailleGrille: number = TAILLE_TEST;
+        private generateurSquelette: GenerateurSquelette = new GenerateurSquelette;
 
         constructor() {
             this.initMatrice();
         }
 
         private initMatrice(): void {
-            this.grille = new Array(this.tailleGrille).fill(VIDE);
-            for (let i = 0; i < this.tailleGrille; i++) {
-                this.grille[i] = new Array(this.tailleGrille).fill(VIDE);
-            }
             this.listeMot = new Array<Mockword>();
+            this.grille = this.generateurSquelette.obtenirSqueletteGrille();
         }
 
         private nettoyerMots(): void {
@@ -35,92 +35,9 @@ module Route {
             }
         }
 
-        // ratioVoulu: float entre 0 et 1
-        private genererCasesNoires(ratioVoulu: number): number {
-            // Verifier une bonne entree
-            if (ratioVoulu < 0 || ratioVoulu > 1) {
-            return null;
-            }
-
-            const nombreCases = Math.ceil(this.tailleGrille * this.tailleGrille * ratioVoulu);
-            let compteurCasesNoires = 0;
-            let x = 0;
-            let y = 0;
-
-            while (compteurCasesNoires < nombreCases) {
-                // On genere une position aleatoire
-                x = Math.floor(Math.random() * this.tailleGrille);
-                y = Math.floor(Math.random() * this.tailleGrille);
-                if (this.verifCaseNoire(x, y)) {
-                    this.grille[y][x] = NOIR;
-                    compteurCasesNoires++;
-                }
-            }
-
-            return nombreCases;
-        }
-
-        private verifCaseNoire(positionX: number, positionY: number): boolean {
-
-            if (this.grille[positionY][positionX] === NOIR) {
-                return false;
-            }
-
-            this.grille[positionY][positionX] = NOIR;
-            let caseDisponible = true;
-
-            caseDisponible = this.neGenerePasDeTrou(positionX - 1, positionY);
-            if (caseDisponible) {
-                caseDisponible = this.neGenerePasDeTrou(positionX + 1, positionY);
-            }
-            if (caseDisponible) {
-                caseDisponible = this.neGenerePasDeTrou(positionX, positionY - 1);
-            }
-            if (caseDisponible) {
-                caseDisponible = this.neGenerePasDeTrou(positionX, positionY + 1);
-            }
-
-            this.grille[positionY][positionX] = VIDE;
-
-            return caseDisponible;
-        }
-
-        private neGenerePasDeTrou(positionX: number, positionY: number): boolean {
-
-            if (positionX < 0 || positionY < 0 || positionX >= this.tailleGrille || positionY >= this.tailleGrille ) {
-                return true;
-            }
-            if (this.grille[positionY][positionX] === NOIR) {
-                return true;
-            }
-            if (positionY - 1 > 0) {
-                if (this.grille[positionY - 1][positionY] === VIDE) {
-                    return true;
-                }
-            }
-            if (positionY + 1 < this.tailleGrille) {
-                if (this.grille[positionY + 1][positionX] === VIDE) {
-                    return true;
-                }
-            }
-            if (positionX - 1 > 0) {
-                if (this.grille[positionY][positionX - 1] === VIDE) {
-                    return true;
-                }
-            }
-            if (positionX + 1 < this.tailleGrille) {
-                if (this.grille[positionY][positionX + 1] === VIDE) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
         /* FONCTION BIDON POUR TESTER DES CHOSES */
         public afficheGrille(req: Request, res: Response, next: NextFunction): void {
             this.initMatrice();
-            this.initCasesNoires(POURCENTAGE_TEST);
             this.initListeMot();
             this.remplirLaGrilleDeMots(0);
 
@@ -243,7 +160,6 @@ module Route {
         private affecterMot(listeMot: Mot[], motAChanger: Mockword): Mot[] {
             // regarder avec simon si on doit trouver un mot en particulier dans la liste
             const index = Math.floor( Math.random() * listeMot.length );
-
             motAChanger.setMot(listeMot[index].mot);
             motAChanger.setDefinition(listeMot[index].definitions[0].definition);
             // console.log(this.listeMot);
@@ -256,12 +172,6 @@ module Route {
             res.send(JSON.stringify(this.listeMot));
         }
 
-        // Interface pour tests...
-        public initCasesNoires(ratioVoulu: number): number {
-            this.initMatrice();
-
-            return this.genererCasesNoires(ratioVoulu);
-        }
     }
 }
 

9f051fb97550e7664a5bc594a792b7e13428f3c7
Sebastien Cadorette <cado.94@me.com>
2018-02-05 21:11:26 -0500

    SC : Remplacement des guillemets doubles à simples.

--- client/src/app/crosswords/service-Requete-de-Grille/requete-de-grille.service.spec.ts
+++ client/src/app/crosswords/service-Requete-de-Grille/requete-de-grille.service.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed, inject } from "@angular/core/testing";
-import { RequeteDeGrilleService } from "./requete-de-grille.service";
+import { TestBed, inject } from '@angular/core/testing';
+import { RequeteDeGrilleService } from './requete-de-grille.service';
 
-describe("RequeteDeGrilleService", () => {
+describe('RequeteDeGrilleService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [RequeteDeGrilleService]
     });
   });
 
-  it("should be created", inject([RequeteDeGrilleService], (service: RequeteDeGrilleService) => {
+  it('should be created', inject([RequeteDeGrilleService], (service: RequeteDeGrilleService) => {
     expect(service).toBeTruthy();
   }));
 });

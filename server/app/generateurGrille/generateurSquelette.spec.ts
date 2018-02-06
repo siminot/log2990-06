b05bbe85589488e274f1a62c046c4e05b4c6ea26
willylab <will.laberge16@gmail.com>
2018-02-05 20:37:03 -0500

    WL: les test de genSquelette testent la meme grille

--- server/app/generateurGrille/generateurSquelette.spec.ts
+++ server/app/generateurGrille/generateurSquelette.spec.ts
@@ -9,6 +9,8 @@ import { GenerateurSquelette } from "./generateurSquelette";
 
     describe("Tests du squelette", () => {
 
+        const grille = genSquelette.getSqueletteGrille();
+
         it ("Le constructeur du squelette devrait fonctionner", (done) => {
             assert.ok(new GenerateurSquelette(TAILLE_GRILLE, POURCENTAGE_CASES_NOIRES));
             done();
@@ -20,7 +22,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Le squelette devrait etre rempli de case noire et blanches", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let correct = true;
             for (let i = 0; i < TAILLE_GRILLE; ++i) {
                 for (let j = 0; j < TAILLE_GRILLE; ++j) {
@@ -34,7 +35,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Le squelette devrait contenir 0.25*10*10 = 25 cases noires", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let compteurCasesNoires = 0;
             for (let i = 0; i < TAILLE_GRILLE; ++i) {
                 for (let j = 0; j < TAILLE_GRILLE; ++j) {
@@ -48,7 +48,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(1)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             for (let i = 1; i < TAILLE_GRILLE - 1; ++i) {
                 for (let j = 1; j < TAILLE_GRILLE - 1; ++j) {
@@ -64,7 +63,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(2)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             if (grille[0][0] === VIDE && grille[0][1] === NOIR && grille[1][0] === NOIR) {
                 estCorrect = false;
@@ -74,7 +72,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(3)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             if (grille[0][TAILLE_GRILLE - 1] === VIDE && grille[0][TAILLE_GRILLE - 2] === NOIR
                 && grille[1][TAILLE_GRILLE - 1] === NOIR) {
@@ -85,7 +82,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(4)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             if (grille[TAILLE_GRILLE - 1][0] === VIDE && grille[TAILLE_GRILLE - 2][0] === NOIR
                 && grille[TAILLE_GRILLE - 1][1] === NOIR) {
@@ -96,7 +92,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(5)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             if (grille[TAILLE_GRILLE - 1][TAILLE_GRILLE - 1] === VIDE && grille[TAILLE_GRILLE - 2][TAILLE_GRILLE - 1] === NOIR
                 && grille[TAILLE_GRILLE - 1][TAILLE_GRILLE - 2] === NOIR) {
@@ -107,7 +102,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(6)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             for (let i = 1; i < TAILLE_GRILLE - 1; ++i) {
                 if (grille[0][i] === VIDE && grille[1][i] === NOIR
@@ -120,7 +114,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(7)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             for (let i = 1; i < TAILLE_GRILLE - 1; ++i) {
                 if (grille[TAILLE_GRILLE - 1][i] === VIDE && grille[TAILLE_GRILLE - 2][i] === NOIR
@@ -133,7 +126,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(8)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             for (let j = 1; j < TAILLE_GRILLE - 1; ++j) {
                 if (grille[j][0] === VIDE && grille[j][1] === NOIR
@@ -146,7 +138,6 @@ import { GenerateurSquelette } from "./generateurSquelette";
         });
 
         it ("Il ne devrait pas avoir un espace vide entoure de cases noires(9)", (done) => {
-            const grille = genSquelette.getSqueletteGrille();
             let estCorrect = true;
             for (let j = 1; j < TAILLE_GRILLE - 1; ++j) {
                 if (grille[j][TAILLE_GRILLE - 1] === VIDE && grille[j][TAILLE_GRILLE - 2] === NOIR

4c599b695fa6a7ab2357d4cc3f1c46b74c74f39d
Sebastien Cadorette <cado.94@me.com>
2018-02-05 22:37:18 -0500

    SC : Arrangement du fichier avec TSLint. Ajout de mots-clés, d'espaces & de retours à la ligne.

--- client/src/app/crosswords/definition/definition.component.ts
+++ client/src/app/crosswords/definition/definition.component.ts
@@ -3,34 +3,34 @@ import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Subscription } from 'rxjs/Subscription';
 
 import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-grille.service';
-import { Word } from '../mockObject/word';
-import { LettreGrille } from '../mockObject/word';
-
+import { Word, LettreGrille } from '../mockObject/word';
 
 @Component({
   selector: 'app-definition',
   templateUrl: './definition.component.html',
-  styleUrls: ['./definition.component.css'],
-  
+  styleUrls: ['./definition.component.css']
 })
 
-
-export class DefinitionComponent implements OnDestroy, OnInit {
+export class DefinitionComponent implements OnInit, OnDestroy {
   private mots: Word[];
   private matriceDesMotsSurGrille: Array<Array<LettreGrille>>;
   private subscriptionMots: Subscription;
   private subscriptionMatrice: Subscription;
-  private reponse: String; 
+  private reponse: String;
   private motSelectionne: Word;
-  
-  constructor (private listeMotsService: RequeteDeGrilleService) {
+
+  public constructor (private listeMotsService: RequeteDeGrilleService) {
     this.mots = this.listeMotsService.getMots();
     this.matriceDesMotsSurGrille = this.listeMotsService.getMatrice();
-    this.subscriptionMots = this.listeMotsService.serviceReceptionMots().subscribe(mots => this.mots = mots);
-    this.subscriptionMatrice = this.listeMotsService.serviceReceptionMatriceLettres().subscribe(matrice => this.matriceDesMotsSurGrille = matrice);
+
+    this.subscriptionMots = this.listeMotsService.serviceReceptionMots()
+      .subscribe((mots) => this.mots = mots);
+
+    this.subscriptionMatrice = this.listeMotsService.serviceReceptionMatriceLettres()
+      .subscribe((matrice) => this.matriceDesMotsSurGrille = matrice);
   }
 
-  ngOnInit() { }
+  public ngOnInit(): void { }
 
   public envoieMots(): void {
     this.listeMotsService.serviceEnvoieMots(this.mots);
@@ -52,8 +52,8 @@ export class DefinitionComponent implements OnDestroy, OnInit {
   }
 
   public changementMotSelectionne(mot: Word): void {
-    for(let mot of this.mots) {
-      mot.activer = false;
+    for (const item of this.mots) {
+      item.activer = false;
     }
     this.motSelectionne = mot;
     mot.activer = !mot.activer;

4b30c37d1a2ff98407e0a3d2ecc2d79aea665ee9
SebastienCadorette <cado.94@me.com>
2018-02-12 20:07:10 -0500

    SC : Ajout du composant Grille et du service de requete de grille pour éviter que les tests plantent.

--- client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
+++ client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
@@ -1,8 +1,12 @@
 import { DirectiveFocusDirective } from './directive-focus.directive';
+import { GrilleComponent } from '../grille/grille.component';
+import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-grille.service';
 
 describe('DirectiveFocusDirective', () => {
   it('should create an instance', () => {
-    const directive = new DirectiveFocusDirective();
+    const service = new RequeteDeGrilleService();
+    const grilleComp = new GrilleComponent(service);
+    const directive = new DirectiveFocusDirective(grilleComp);
     expect(directive).toBeTruthy();
   });
 });

e058c14ca43ed518f1a2ed882c206696afd93943
Sebastien Cadorette <cado.94@me.com>
2018-02-16 09:11:35 -0500

    SC : Ajustement des tests pour qu'il n'y ait plus d'erreurs.

--- client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
+++ client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
@@ -1,12 +1,8 @@
 import { DirectiveFocusDirective } from './directive-focus.directive';
-import { GrilleComponent } from '../grille/grille.component';
-import { RequeteDeGrilleService } from '../service-Requete-de-Grille/requete-de-grille.service';
 
 describe('DirectiveFocusDirective', () => {
   it('should create an instance', () => {
-    const service = new RequeteDeGrilleService();
-    const grilleComp = new GrilleComponent(service);
-    const directive = new DirectiveFocusDirective(grilleComp);
+    const directive = new DirectiveFocusDirective();
     expect(directive).toBeTruthy();
   });
 });

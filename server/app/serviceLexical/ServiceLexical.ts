b8d14b7111b711c83ff42667ffa54c0aa8f355f6
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-02-20 12:13:49 -0500

    ML: finalisation des corrections serviceLexical

--- server/app/serviceLexical/ServiceLexical.ts
+++ server/app/serviceLexical/ServiceLexical.ts
@@ -42,61 +42,6 @@ module moduleServiceLexical {
             .then((motsAPI: MotAPI[]) => this.renvoyerResultat(this.obtenirMotsFormattes(motsAPI, frequence), res))
             .catch(() => res.send(new Error(MESSAGE_ERREUR_API_EXTERNE)));
         }
-
-        // Méthodes pas utilisées... à effacer?
-
-        // Services de mots
-
-        // Obtention des définitions d'un seul mot
-        /* public servirDefinitionsMot(mot: string, res: Response): void {
-            if (this.estUnMotValide(mot)) {
-                this.obtenirMotsDeLAPI(mot, 1)
-                    .then((dictionnaire: Mot[]) => {
-                        if ((dictionnaire.length !== 0) && (dictionnaire[0].definitions !== null)) {
-                            res.send(dictionnaire[0]);
-                        } else {
-                            res.send(MESSAGE_AUCUN_RESULTAT);
-                        }
-                    })
-                    .catch(() => { res.send(new Error(MESSAGE_ERREUR_API_EXTERNE)); });
-            } else {
-                throw new Error(MESSAGE_REQUETE_INVALIDE);
-            }
-        }
-
-        private estUnMotValide(mot: string): boolean {
-            for (let i = 0; i < mot.length; i++) {
-                if (!mot.toLowerCase().charAt(i).match(/[a-z]/)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        // Obtention des mots selon la longueur
-        public servirMotsSelonLongueur(longueur: string, frequence: Frequence, res: Response): void {
-            const LONGUEUR: number = Number.parseInt(longueur);
-            if (!isNaN(LONGUEUR)) {
-                this.obtenirMotsFormattes(this.obtenirContrainteLongueur(LONGUEUR))
-                    .then((dictionnaire: Mot[]) => {
-                        this.renvoyerMots(this.trierMotsSelonFrequence(dictionnaire, frequence), res);
-                    })
-                    .catch(() => { res.send(Error(MESSAGE_ERREUR_API_EXTERNE)); });
-            } else {
-                throw new Error(MESSAGE_REQUETE_INVALIDE);
-            }
-        }
-
-        private obtenirContrainteLongueur(longueur: number): string {
-            let contrainte = "";
-
-            for (let i = 0; i < longueur; i++) {
-                contrainte += LETTRE_INCONNUE;
-            }
-
-            return contrainte;
-        } */
     }
 }
 
--- server/app/serviceLexical/routeServiceLexical.ts
+++ server/app/serviceLexical/routeServiceLexical.ts
@@ -5,7 +5,6 @@ import Types from "../types";
 import { ServiceLexical } from "./ServiceLexical";
 import { Frequence } from "./Mot";
 import { ServiceWeb } from "../serviceweb";
-// import { read } from "fs";
 
 @injectable()
 export class RouteServiceLexical extends ServiceWeb {
@@ -27,20 +26,6 @@ export class RouteServiceLexical extends ServiceWeb {
             this.serviceLexical.servirMotsSelonContrainte(req.params.contrainte, Frequence.NonCommun, res);
         });
 
-        // Routes pas utilisées... à effacer?
-
-        /* router.get("/commun/longueur/:longueur", (req: Request, res: Response, next: NextFunction) => {
-            this.serviceLexical.servirMotsSelonLongueur(req.params.longueur, Frequence.Commun, res);
-        });
-
-        router.get("/noncommun/longueur/:longueur", (req: Request, res: Response, next: NextFunction) => {
-            this.serviceLexical.servirMotsSelonLongueur(req.params.longueur, Frequence.NonCommun, res);
-        });
-
-        router.get("/def/:mot", (req: Request, res: Response, next: NextFunction) => {
-            this.serviceLexical.servirDefinitionsMot(req.params.mot, res);
-        });
- */
         return router;
     }
 }

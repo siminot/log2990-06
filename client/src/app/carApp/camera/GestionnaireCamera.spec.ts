068bb843c7c1b2348eb05c424e6e4f944f3236db
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-02-20 19:07:03 -0500

    Ajout de test pour GestionnaireCamera et ServiceDeRendu

--- client/src/app/carApp/camera/GestionnaireCamera.spec.ts
+++ client/src/app/carApp/camera/GestionnaireCamera.spec.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TestBed, async } from "@angular/core/testing";
 
 import { GestionnaireCamera } from "./GestionnaireCamera";
 import { CameraJeu } from "./CameraJeu";
@@ -7,38 +7,47 @@ import { CameraJeu3D } from "./CameraJeu3D";
 import { CameraJeu2D } from "./CameraJeu2D";
 import { GestionnaireVoitures } from "../voiture/gestionnaireVoitures";
 
-let gestionnaire: GestionnaireCamera;
-let fixture: ComponentFixture<GestionnaireCamera>;
-
-beforeEach(async() => {
-  TestBed.configureTestingModule({ declarations: [GestionnaireCamera] });
-  fixture = TestBed.createComponent(GestionnaireCamera);
-  gestionnaire = fixture.componentInstance;
-});
-
 describe("Gestionnaire camera", () => {
+  let gestionnaireCamera: GestionnaireCamera;
+  let gestionnaireVoitures: GestionnaireVoitures;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GestionnaireCamera ],
+      providers: [ GestionnaireVoitures ]
+    });
+
+    gestionnaireVoitures = new GestionnaireVoitures();
+    gestionnaireCamera = new GestionnaireCamera(gestionnaireVoitures);
+  }));
+
+  beforeEach(() => {
+  });
 
   describe("Constructeur", () => {
     it("Objet est construit", () => {
-      const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
       expect(gestionnaireCamera).toBeDefined();
     });
 
+    it("Composante utilises sont construites", () => {
+      expect(gestionnaireVoitures).toBeDefined();
+    });
+
     it("Bon nombre de camera initialisees", () => {
-      const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
+      // const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
       const NOMBRE_CAMERAS: number = 2;
       expect(gestionnaireCamera["cameras"].length === NOMBRE_CAMERAS);
     });
 
     it("La premiere camera est 3D", () => {
-      const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
+      // const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
       expect(gestionnaireCamera["cameraCourante"] instanceof CameraJeu3D);
     });
   });
 
   describe("Changement de camera", () => {
     it("Alterner la camera du jeu entre 2D et 3D", () => {
-      const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
+      // const gestionnaireCamera: GestionnaireCamera = new GestionnaireCamera(new GestionnaireVoitures());
       let cameraCourante: CameraJeu;
       if (gestionnaireCamera["cameraCourante"] instanceof CameraJeu3D) {
         gestionnaireCamera.changerCamera();
--- client/src/app/carApp/serviceDeRendu/serviceDeRendu.spec.ts
+++ client/src/app/carApp/serviceDeRendu/serviceDeRendu.spec.ts
@@ -1,16 +1,51 @@
-/* import { TestBed, inject } from "@angular/core/testing";
+import * as assert from "assert";
+import { TestBed, async } from "@angular/core/testing";
 
 import { ServiceDeRendu } from "./serviceDeRendu";
+import { GestionnaireScene } from "../scene/GestionnaireScene";
+import { GestionnaireCamera } from "../camera/GestionnaireCamera";
+import { GestionnaireEcran } from "../ecran/gestionnaireEcran";
+import { GestionnaireSkybox } from "../skybox/gestionnaireSkybox";
+import { GestionnaireVoitures } from "../voiture/gestionnaireVoitures";
 
-describe("ServiceDeRendu", () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [ServiceDeRendu]
-        });
+describe("Service de rendu", () => {
+  let serviceDeRendu: ServiceDeRendu;
+
+  let gestionnaireVoitures: GestionnaireVoitures;
+  let gestionnaireSkybox: GestionnaireSkybox;
+  let gestionnaireScene: GestionnaireScene;
+  let gestionnaireEcran: GestionnaireEcran;
+  let gestionnaireCamera: GestionnaireCamera;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GestionnaireCamera ],
+      providers: [ GestionnaireVoitures ]
     });
 
-    it("should be created", inject([ServiceDeRendu], (service: ServiceDeRendu) => {
-        expect(service).toBeTruthy();
-    }));
+    gestionnaireVoitures = new GestionnaireVoitures();
+    gestionnaireSkybox = new GestionnaireSkybox();
+    gestionnaireScene = new GestionnaireScene(gestionnaireSkybox, gestionnaireVoitures);
+    gestionnaireEcran = new GestionnaireEcran();
+    gestionnaireCamera = new GestionnaireCamera(gestionnaireVoitures);
+
+    serviceDeRendu = new ServiceDeRendu(gestionnaireScene, gestionnaireCamera, gestionnaireEcran);
+  }));
+
+  beforeEach(() => {
+  });
+
+  describe("Constructeur", () => {
+    it("Objet est construit", () => {
+      expect(serviceDeRendu).toBeDefined();
+    });
+
+    it("Composante utilises sont construites", () => {
+      expect(gestionnaireVoitures).toBeDefined();
+      expect(gestionnaireSkybox).toBeDefined();
+      expect(gestionnaireScene).toBeDefined();
+      expect(gestionnaireEcran).toBeDefined();
+      expect(gestionnaireCamera).toBeDefined();
+    });
+  });
 });
- */

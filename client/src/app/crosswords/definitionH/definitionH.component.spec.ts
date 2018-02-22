8ec0f22782b93cebe02e14340bacaa1471d161b7
J-F <jffontaine_@hotmail.com>
2018-02-21 23:14:43 -0500

    rename des nom de fichiers: préparation pour un nouvel component afin d'y mettre les définition horizontale.

---
+++ client/src/app/crosswords/definitionH/definitionH.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DefinitioHComponent } from './definitio-h.component';
+
+describe('DefinitioHComponent', () => {
+  let component: DefinitioHComponent;
+  let fixture: ComponentFixture<DefinitioHComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DefinitioHComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DefinitioHComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/crosswords/definitionH/definitionH.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-definitionh",
+  templateUrl: "./definitionH.component.html",
+  styleUrls: ["./definitionH.component.css"]
+})
+export class DefinitionHComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

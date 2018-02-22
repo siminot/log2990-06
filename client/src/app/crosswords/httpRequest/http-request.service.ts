25709090799602f6d90d3eeb999521179a54912e
J-F <jffontaine_@hotmail.com>
2018-02-21 19:29:41 -0500

    http header enlevé

--- client/src/app/crosswords/httpRequest/http-request.service.ts
+++ client/src/app/crosswords/httpRequest/http-request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient} from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 import { Word} from "../mockObject/word";
 import "rxjs/add/operator/map";

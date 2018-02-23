786441136904c98f2a6c41f7c9c5d18633bbeb57
Samuel Ferron <fersamw@gmail.com>
2018-02-23 10:41:33 -0500

    SF: modif de l'objet Option partie en structure

--- common/communication/optionPartie.ts
+++ common/communication/optionPartie.ts
@@ -1,29 +1,14 @@
 export class OptionPartie {
-    private idPartie: string; // id possiblement pour identifier une partie (en ligne)
-    private nombreJoueur: number;
-    private difficulte: string;
-    private requete: string;
+    public idPartie: number; // id possiblement pour identifier une partie (en ligne)
+    public nombreJoueur: number;
+    public difficulte: string;
+    public requete: string;
 
     constructor() {
-        this.idPartie = "ZERO";
+        this.idPartie = 0;
         this.nombreJoueur = 0;
         this.difficulte = "facile";
         this.requete = "http://localhost:3000/grille/facile";
     }
 
-    public get id(): string {
-        return this.idPartie;
-    }
-
-    public get nbJoueur(): number {
-        return this.nombreJoueur;
-    }
-
-    public get difficulty(): string {
-        return this.difficulte;
-    }
-
-    public get request(): string {
-        return this.requete;
-    }
 }

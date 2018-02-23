214818a11169b972b0e1c0a795d1b1295a9af11d
Sebastien Cadorette <cado.94@me.com>
2018-02-23 10:34:39 -0500

    SC : Arrangement de la structure de données dans le « COMMON ».

--- common/mockObject/mockListWord.ts
+++ common/mockObject/mockListWord.ts
@@ -1,8 +1,8 @@
-import {Mockword} from "./mockWord";
+import { Mockword } from "./mockWord";
 
-export const listeMot: Mockword[] = [
-{mots: "Pout", definition: "Qui aime la poutine", vertical: false, longeur: 4, premierX: 0, premierY: 0, activer: false },
-{mots: "Pome", definition: "provient du pommier", vertical: true, longeur: 4, premierX: 0, premierY: 0, activer: false },
-{mots: "Eppa", definition: "Pas super super", vertical: false, longeur: 4, premierX: 0, premierY: 3, activer: false },
-{mots: "Tata", definition: "ben nice is ben cool", vertical: true, longeur: 4, premierX: 3, premierY: 0, activer: false }
-]
+// export const listeMot: Mockword[] = [
+// {mot: "Pout", definition: "Qui aime la poutine", estVertical: false, longeur: 4, premierX: 0, premierY: 0, estTraite:false, motTrouve:false},
+// {mot: "Pome", definition: "provient du pommier", estVertical: true, longeur: 4, premierX: 0, premierY: 0, estTraite:false, motTrouve:false},
+// {mot: "Eppa", definition: "Pas super super", estVertical: false, longeur: 4, premierX: 0, premierY: 3, estTraite:false, motTrouve:false},
+// {mot: "Tata", definition: "ben nice is ben cool", estVertical: true, longeur: 4, premierX: 3, premierY: 0, estTraite:false, motTrouve:false}
+// ];

46778768dad673288dd674f8d69837889886c496
willylab <will.laberge16@gmail.com>
2018-02-24 15:48:00 -0500

    WL: ajout de tests pour generateurlisteMots

--- server/app/generateurGrille/generateurListeMots.spec.ts
+++ server/app/generateurGrille/generateurListeMots.spec.ts
@@ -3,21 +3,19 @@ import { GenerateurListeMots } from "./generateurListeMots";
 import * as assert from "assert";
 
 {
-
-    // let listeTest: Array<Mockword> = new Array<Mockword>();
+    // tslint:disable: no-magic-numbers
 
     const genListeTest: GenerateurListeMots = new GenerateurListeMots();
 
-    let uneGrille: Array<Array<string>> = new  Array<Array<string>>();
-    uneGrille = [["_", "_", "_", "_", "_"],
-                 ["_", "0", "0", "_", "_"],
-                 ["_", "_", "0", "_", "_"],
-                 ["0", "_", "_", "0", "_"],
-                 ["_", "_", "_", "0", "0"]];
+    const uneGrille =  [["_", "_", "_", "_", "_"],
+                        ["_", "0", "0", "_", "_"],
+                        ["_", "_", "0", "_", "_"],
+                        ["0", "_", "_", "0", "_"],
+                        ["_", "_", "_", "0", "0"]];
 
     describe("Tests GenerateurListeMots", () => {
 
-        describe("- tests generation des mots", () => {
+        describe("- Tests generation des mots", () => {
 
             it("- Le constructeur du mot devrait fonctionner", (done) => {
                 const LONGUEUR_MOT = 4;
@@ -30,15 +28,93 @@ import * as assert from "assert";
                 done();
             });
 
-            // it("- Devrait faire un mot horizontal", (done) => {
-            //     assert.ok(genTest.genererMot(0, 0, false));
-            //     done();
-            // });
+        });
+        describe("- Tests pour une grille quelconque", () => {
 
-            // it("- Devrait causer une erreur", (done) => {
-            //     assert.throws(genTest.genererMot , /-1, 0, false/, "quoi");
-            //     done();
-            // });
+            const listeMotsTest = genListeTest.donnerUneListe(uneGrille);
+
+            it("- Devrait avoir 11 mots", (done) => {
+                assert(listeMotsTest.length === 11);
+                done();
+            });
+
+            it("- Devrait avoir 6 mots horizontaux", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (!mot.getVertical()) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 6);
+                done();
+            });
+
+            it("- Devrait avoir 5 mots verticaux", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getVertical()) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 5);
+                done();
+            });
+
+            it("- Devrait avoir 0 mots de 1 lettres", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getLongueur() === 1) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 0);
+                done();
+            });
+
+            it("- Devrait avoir 5 mots de 2 lettres", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getLongueur() === 2) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 5);
+                done();
+            });
+
+            it("- Devrait avoir 4 mots de 3 lettres", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getLongueur() === 3) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 4);
+                done();
+            });
+
+            it("- Devrait avoir 1 mot de 4 lettres", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getLongueur() === 4) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 1);
+                done();
+            });
+
+            it("- Devrait avoir 0 mot de 6 lettres", (done) => {
+                let compteur = 0;
+                for (const mot of listeMotsTest) {
+                    if (mot.getLongueur() === 6) {
+                        compteur++;
+                    }
+                }
+                assert(compteur === 0);
+                done();
+            });
+
 
         });
 

e414a7b89a5906745502223994a37133eeae1ed2
Samuel Ferron <fersamw@gmail.com>
2018-02-24 16:11:37 -0500

    SF: Ajoute d'une fonction et des tests

--- server/app/generateurGrille/genSquelette.spec.ts
+++ server/app/generateurGrille/genSquelette.spec.ts
@@ -5,6 +5,7 @@ import * as assert from "assert";
 {
 
     const monGen: GenSquelette = new GenSquelette();
+    const indiceBidon = 5;
 
     describe("Tests GenSquelette", () => {
 
@@ -22,6 +23,41 @@ import * as assert from "assert";
                 });
             }
 
+        });
+
+        describe("Test prob", () => {
+            it("devrait etre faux", (done) => {
+                assert.equal(false, monGen["probabiliterDeContinuerMot"](0, -1));
+                done();
+            });
+
+            it("devrait etre faux", (done) => {
+                assert.equal(false, monGen["probabiliterDeContinuerMot"](0, lesConst.TAILLE_TABLEAU));
+                done();
+            });
+
+            it("devrait toujours etre vrai si premier indice est 0", (done) => {
+                assert.equal(true, monGen["probabiliterDeContinuerMot"](0, indiceBidon));
+                done();
+            });
+
+        });
+
+        describe("Test indicePasDansGrille", () => {
+            it("devrait etre Vrai", (done) => {
+                assert.equal(true, monGen["indicePasDansGrille"](-1));
+                done();
+            });
+
+            it("devrait etre Vrai", (done) => {
+                assert.equal(true, monGen["indicePasDansGrille"](lesConst.TAILLE_TABLEAU));
+                done();
+            });
+
+            it("devrait etre Faux", (done) => {
+                assert.equal(false, monGen["indicePasDansGrille"](indiceBidon));
+                done();
+            });
         });
 
     });
--- server/app/generateurGrille/genSquelette.ts
+++ server/app/generateurGrille/genSquelette.ts
@@ -1,4 +1,4 @@
-import { TAILLE_TABLEAU, NOIR } from "./constantes";
+import { TAILLE_TABLEAU, NOIR, HEURISTIQUE_LNG_MOT } from "./constantes";
 
 export class GenSquelette {
 
@@ -8,7 +8,7 @@ export class GenSquelette {
         this.grille = new Array<Array<string>>();
         this.initGrille();
         // TEMPORAIRE POUR COMPILATION
-        this.probabiliterDeContinuerMot(0);
+        this.probabiliterDeContinuerMot(0, 0);
     }
 
     private initGrille(): void {
@@ -18,9 +18,44 @@ export class GenSquelette {
         }
     }
 
+    // Retourne l'indice ou le mot se termine
+    // private genererEspaceMot(indiceDep: number): number {
+
+    //     let indice = 0;
+
+    //     while (this.probabiliterDeContinuerMot(indice, indiceDep)) {
+    //         indice++;
+    //     }
+
+    //     // for (let i = 0; i < (TAILLE_TABLEAU - indiceDep); i++) {
+    //     //     if (this.probabiliterDeContinuerMot(i, indiceDep)) {
+
+    //     //     }
+    //     // }
+
+    //     return 0;
+    // }
+
     // TESTER QUAND ON VA AVOIR UNE BONNE HEURISTIQUE
-    private probabiliterDeContinuerMot(i: number): number {
-        return 0;
+    private probabiliterDeContinuerMot(indice: number, indiceDep: number): boolean {
+        if (this.indicePasDansGrille(indice) || this.indicePasDansGrille(indiceDep)) {
+            return false;
+        }
+
+        const prob = 1 - indice / (HEURISTIQUE_LNG_MOT * (TAILLE_TABLEAU - indiceDep));
+
+        return Math.random() < prob ? true : false;
+    }
+
+    private indicePasDansGrille(indice: number): boolean {
+        if (indice < 0) {
+            return true;
+        } else if (indice >= TAILLE_TABLEAU) {
+            return true;
+        }
+
+        // Sinon dans grille
+        return false;
     }
 
 }

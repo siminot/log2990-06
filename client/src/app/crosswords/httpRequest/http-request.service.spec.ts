e9c9bf812bb214433321cba3bf1bcd1648c8b9a3
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-02-24 13:58:26 -0500

    Modification de tests dans Crossword qui empechait Karma de compiler

--- client/src/app/crosswords/httpRequest/http-request.service.spec.ts
+++ client/src/app/crosswords/httpRequest/http-request.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from "@angular/core/testing";
-
-import { HttpRequestService } from "./http-request.service";
+import { HttpClient} from "@angular/common/http";
+import { HttpeReqService } from "./http-request.service";
 
 describe("HttpRequestService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpRequestService]
+      providers: [HttpeReqService]
     });
   });
 
-  it("should be created", inject([HttpRequestService], (service: HttpRequestService) => {
+  it("should be created", inject([HttpClient], (service: HttpeReqService) => {
     expect(service).toBeTruthy();
   }));
 });

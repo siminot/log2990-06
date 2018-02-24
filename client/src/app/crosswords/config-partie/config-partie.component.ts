bbb83c31f95c8747d8119723fd2fd5b3a3835b62
Samuel Ferron <fersamw@gmail.com>
2018-02-24 14:41:36 -0500

    SF: conflits

--- client/src/app/crosswords/config-partie/config-partie.component.ts
+++ client/src/app/crosswords/config-partie/config-partie.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-<<<<<<< HEAD
 import { HttpeReqService } from "../httpRequest/http-request.service";
 import { OptionPartie } from "./../../../../../common/communication/optionPartie";
 const REQUETE_INIT: string = "http://localhost:3000/grille/";
-=======
->>>>>>> develop
 
 @Component({
   selector: "app-config-partie",
@@ -13,7 +10,6 @@ const REQUETE_INIT: string = "http://localhost:3000/grille/";
 })
 export class ConfigPartieComponent implements OnInit {
 
-<<<<<<< HEAD
   private requete: string; // Changer pour une constante
   private lesOptions: OptionPartie = new OptionPartie;
 
@@ -51,11 +47,6 @@ export class ConfigPartieComponent implements OnInit {
     }
     this.lesOptions.difficulte = ajout;
     this.requete += ajout;
-=======
-  public constructor() { }
-
-  public ngOnInit(): void {
->>>>>>> develop
   }
 
 }

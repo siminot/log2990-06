c69f3cf2286947806618acf1e62feebd52ed8606
Samuel Ferron <fersamw@gmail.com>
2018-02-24 14:50:13 -0500

    SF: reglage de petit probleme

--- server/app/generateurGrille/routeGenGrille.ts
+++ server/app/generateurGrille/routeGenGrille.ts
@@ -24,7 +24,7 @@ export class RouteGenGrille {
                    (req: Request, res: Response, next: NextFunction) => this.genGrille.getMots(req, res, next));
 
         router.get("/facile",
-                   (req: Request, res: Response, next: NextFunction) => this.genGrille.prototypeRequete(req, res, next));
+                   async (req: Request, res: Response, next: NextFunction) => this.genGrille.prototypeRequete(req, res, next));
 
         router.get("/normal",
                    (req: Request, res: Response, next: NextFunction) => this.genGrille.afficheDifficile(req, res, next));

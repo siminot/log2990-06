e77f48d1ad3bcd151d0665d220a13c4abb330280
SebastienCadorette <cado.94@me.com>
2018-02-25 10:46:53 -0500

    SC : Communication avec le service permettant d'accéder au pointage.

--- client/src/app/crosswords/info-joueur1/info-joueur1.component.ts
+++ client/src/app/crosswords/info-joueur1/info-joueur1.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs/Subscription";
+import { InfojoueurService } from "../service-info-joueur/infojoueur.service";
 
 @Component({
   selector: "app-info-joueur1",
   templateUrl: "./info-joueur1.component.html",
   styleUrls: ["./info-joueur1.component.css"]
 })
-export class InfoJoueur1Component implements OnInit {
+export class InfoJoueur1Component implements OnInit, OnDestroy {
+  private _pointage: number;
+  private _subscriptionPointage: Subscription;
 
-  public constructor() { }
+  public constructor(private _servicePointage: InfojoueurService) {
+    this._pointage = 0;
+   }
 
   public ngOnInit(): void {
+    this._subscriptionPointage = this._servicePointage.serviceReceptionPointage()
+      .subscribe((pointage) => {
+        this._pointage = pointage;
+      })
+  }
+
+  public ngOnDestroy(): void {
+    this._subscriptionPointage.unsubscribe();
   }
 }

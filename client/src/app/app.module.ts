99607f3f989d6dbe9513708176f57630134fb11e
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-02-25 19:52:01 -0500

    Restructuration du composante Definition

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,11 +16,10 @@ import { GestionnaireEcran } from "./carApp/ecran/gestionnaireEcran";
 import { AppRoutingModule } from ".//app-routing.module";
 import { HttpeReqService } from "./crosswords/httpRequest/http-request.service";
 
-import { DefinitionHComponent} from "./crosswords/definitionH/definitionH.component";
-import { DefinitionVComponent} from "./crosswords/definitionV/definitionV.component";
+import { DefinitionHComponent} from "./crosswords/definition/definitionH.component";
+import { DefinitionVComponent} from "./crosswords/definition/definitionV.component";
 import { MainGrilleComponent } from "./crosswords/main-grille/main-grille.component";
 import { GrilleComponent } from "./crosswords/grille/grille.component";
-import { DefinitionComponent } from "./crosswords/definition/definition.component";
 import { RequeteDeGrilleService } from "./crosswords/service-Requete-de-Grille/requete-de-grille.service";
 import { ConfigPartieComponent } from "./crosswords/config-partie/config-partie.component";
 import { DirectiveFocusDirective } from "./crosswords/directive-focus/directive-focus.directive";
@@ -33,7 +32,6 @@ import { InfojoueurService } from "./crosswords/service-info-joueur/infojoueur.s
         GameComponent,
         MainGrilleComponent,
         GrilleComponent,
-        DefinitionComponent,
         ConfigPartieComponent,
         DirectiveFocusDirective,
         DefinitionHComponent,

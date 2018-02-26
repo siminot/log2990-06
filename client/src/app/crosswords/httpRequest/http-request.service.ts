da9cfd28c18edf63b36bb13c3cfc3b0121c5719a
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-02-25 22:03:21 -0500

    Formatage du fichier http-request

--- client/src/app/crosswords/httpRequest/http-request.service.ts
+++ client/src/app/crosswords/httpRequest/http-request.service.ts
@@ -8,15 +8,14 @@ import "rxjs/add/operator/catch";
 @Injectable()
 export class HttpeReqService {
   private url: string = "http://localhost:3000/grille/facile";
+
   public constructor(private http: HttpClient) { }
 
   public getWord(): Observable<Word[] > {
-
     return this.http.get<Word[]>(this.url);
   }
 
   public modifierRequete(nouvelleRequete: string): void {
     this.url = nouvelleRequete;
   }
-
 }

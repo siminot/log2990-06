a74aa2e7e66a3d78207e5f578a7d3b92ef899ed6
willylab <will.laberge16@gmail.com>
2018-02-26 11:51:53 -0500

    WL: rajouter typedef dans generateurGrille.spec

--- server/app/generateurGrille/generateurGrille.spec.ts
+++ server/app/generateurGrille/generateurGrille.spec.ts
@@ -7,21 +7,21 @@ describe("Tests GenerateurGrille", () => {
 
     describe("- tests du constructeur", () => {
 
-        it("bon fonctionnement du constructeur", (done) => {
+        it("bon fonctionnement du constructeur", (done: MochaDone) => {
             assert.ok(new GenerateurGrille);
             done();
         });
 
-        const generateurGrille = new GenerateurGrille();
+        const generateurGrille: GenerateurGrille = new GenerateurGrille();
 
-        it("le constructeur cree bien des options", (done) => {
+        it("le constructeur cree bien des options", (done: MochaDone) => {
             assert.ok(generateurGrille["optionsPartie"]);
             done();
         });
 
-        const TEMPS_MAXIMAL = 30000; // une grille doit etre retournee en moins de 30 secondes
+        const TEMPS_MAXIMAL: number = 30000; // une grille doit etre retournee en moins de 30 secondes
         it("requeteGrille fonctionne (le serveur doit fonctionner)", async () => {
-            const URL_TEST = "http://localhost:3000/grille/facile";
+            const URL_TEST: string = "http://localhost:3000/grille/facile";
             const data: Mockword = await WebRequest.json<Mockword>(URL_TEST);
             assert.ok(data);
 

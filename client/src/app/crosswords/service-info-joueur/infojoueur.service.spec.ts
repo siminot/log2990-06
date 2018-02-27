94e04b4b9721c60fcc0b18675131e39f32bd2d06
Sebastien Cadorette <cado.94@me.com>
2018-02-26 19:23:30 -0500

    SC : Début de test pour le service info joueur.

--- client/src/app/crosswords/service-info-joueur/infojoueur.service.spec.ts
+++ client/src/app/crosswords/service-info-joueur/infojoueur.service.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import { InfojoueurService } from "./infojoueur.service";
 
 describe("InfojoueurService", () => {
+  let serviceInfoJoueur: InfojoueurService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [InfojoueurService]
     });
   });
 
+  beforeEach(() => {
+    serviceInfoJoueur = new InfojoueurService();
+  });
+
   it("should be created", inject([InfojoueurService], (service: InfojoueurService) => {
     expect(service).toBeTruthy();
   }));
+
+  it("Incrémentation du nombre de mot découvert", () => {
+    const incrementationPointage: number = 10;
+    serviceInfoJoueur.incrementationNbMotDecouv(incrementationPointage);
+    expect(serviceInfoJoueur["_nbMotsDecouverts"]).toEqual(incrementationPointage);
+  });
 });

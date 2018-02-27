7174d90dc042134692004b15fbd075e95d4dfcaf
willylab <will.laberge16@gmail.com>
2018-02-26 19:00:37 -0500

    WL: typedef generateurListeMot

--- server/app/generateurGrille/generateurListeMots.ts
+++ server/app/generateurGrille/generateurListeMots.ts
@@ -15,7 +15,7 @@ export class GenerateurListeMots {
 
     // Sort la liste en ordre decroissant et retire les mots de 1 lettres
     private nettoyerMots(): void {
-        this.listeMot.sort((n1, n2) => n2.getLongueur() - n1.getLongueur());
+        this.listeMot.sort((n1: MotGenerationGrille, n2: MotGenerationGrille) => n2.getLongueur() - n1.getLongueur());
         while (this.listeMot[this.listeMot.length - 1].getLongueur() === 1) {
             this.listeMot.pop();
         }
@@ -27,8 +27,8 @@ export class GenerateurListeMots {
             throw new Error("Entree negative interdite");
         }
 
-        let longMot = 0;
-        let mot = "";
+        let longMot: number = 0;
+        let mot: string = "";
         for (let i: number = estVertical ? y : x; i < uneGrille.length; i++) {
             if (uneGrille[i][x] !== NOIR && estVertical) {
                 longMot++;
@@ -48,9 +48,9 @@ export class GenerateurListeMots {
 
     private genererListeMot(uneGrille: Array<Array<string>>): number {
 
-        let ctrMots = 0;
-        for (let i = 0; i < uneGrille.length; i++) {
-            for (let j = 0; j < uneGrille.length; j++) {
+        let ctrMots: number = 0;
+        for (let i: number = 0; i < uneGrille.length; i++) {
+            for (let j: number = 0; j < uneGrille.length; j++) {
                 if (uneGrille[i][j] === VIDE) {
                     if (j === 0) {
                         this.listeMot.push(this.genererMot(j, i, false, uneGrille));

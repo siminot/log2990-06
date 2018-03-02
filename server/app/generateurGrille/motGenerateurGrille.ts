1d189a046a400e6f1e5204932250141d0130eae0
willylab <will.laberge16@gmail.com>
2018-03-02 09:15:17 -0500

    WL: modfication mot gen grille avec bon set et get

--- server/app/generateurGrille/motGenerateurGrille.ts
+++ server/app/generateurGrille/motGenerateurGrille.ts
@@ -1,71 +1,70 @@
-const PAS_IMPORTANCE: number = 20;
 const CARACTERE_INCONNU: string = "_";
 
 export class MotGenerationGrille {
 
-    private mot: string;
-    private definition: string;
-    private estVertical: boolean;
-    private longeur: number;
-    private premierX: number;
-    private premierY: number;
-    private estTraite: boolean;
+    private _mot: string;
+    private _definition: string;
+    private _estVertical: boolean;
+    private _longueur: number;
+    private _premierX: number;
+    private _premierY: number;
+    private _estTraite: boolean;
 
     public constructor(estVertical: boolean, longueur: number, premierX: number, premierY: number) {
-        this.mot = "";
-        this.definition = "";
-        this.estVertical = estVertical;
-        this.longeur = longueur;
-        this.premierX = premierX;
-        this.premierY = premierY;
-        this.estTraite = false;
+        this._mot = "";
+        this._definition = "";
+        this._estVertical = estVertical;
+        this._longueur = longueur;
+        this._premierX = premierX;
+        this._premierY = premierY;
+        this._estTraite = false;
 
     }
     // getters
-    public getVertical(): boolean {
-        return this.estVertical;
+    public get estVertical(): boolean {
+        return this._estVertical;
     }
 
-    public getLongueur(): number {
-        return this.longeur;
+    public get longueur(): number {
+        return this._longueur;
     }
 
-    public getPremierX(): number {
-        return this.premierX;
+    public get premierX(): number {
+        return this._premierX;
     }
 
-    public getPremierY(): number {
-        return this.premierY;
+    public get premierY(): number {
+        return this._premierY;
     }
 
-    public getMot(): string {
-        return this.mot;
+    public get mot(): string {
+        return this._mot;
     }
 
-    public getDefinition(): string {
-        return this.definition;
+    public set mot(mot: string) {
+        this._mot = mot;
     }
 
-    public getEstTraite(): boolean {
-        return this.estTraite;
+    public get definition(): string {
+        return this._definition;
     }
 
-    // setters
-    public setMot(mot: string): void {
-        this.mot = mot;
-    }
-    public setDefinition (definition: string): void {
+    public set definition(definition: string) {
         this.definition = definition;
     }
 
+    public get estTraite(): boolean {
+        return this._estTraite;
+    }
+
     public setEstTraite(etat: boolean): void {
-        this.estTraite = etat;
+        this._estTraite = etat;
     }
 
     public inverserXY(): void {
         const temp: number = this.premierX;
-        this.premierX = this .premierY;
-        this.premierY = temp;
+        this._premierX = this .premierY;
+        this._premierY = temp;
     }
 
     // autre
@@ -90,34 +89,34 @@ export class MotGenerationGrille {
     }
 
     private estMemeSens(autreMot: MotGenerationGrille): boolean {
-        return this.getVertical() === autreMot.getVertical();
+        return this.estVertical === autreMot.estVertical;
     }
 
     private get coordonneeVariableMin(): number {
         if (this.estVertical) {
-            return this.getPremierY();
+            return this.premierY;
         } else {
-            return this.getPremierX();
+            return this.premierX;
         }
     }
 
     private get coordonneeVariableMax(): number {
-        return this.coordonneeVariableMin + this.getLongueur() - 1;
+        return this.coordonneeVariableMin + this.longueur - 1;
     }
 
     private get coordonneeFixe(): number {
         if (this.estVertical) {
-            return this.getPremierX();
+            return this.premierX;
         } else {
-            return this.getPremierY();
+            return this.premierY;
         }
     }
 
     private coordonneeVariableMinAutreMot(autre: MotGenerationGrille): number {
         if (autre.estVertical) {
-            return this.getPremierY();
+            return this.premierY;
         } else {
-            return this.getPremierX();
+            return this.premierX;
         }
     }
 
@@ -129,6 +128,7 @@ export class MotGenerationGrille {
             }
         }
         if (this.estLieAvecAutreMot(ancienMot)) {
+            const PAS_IMPORTANCE: number = 20;
             i += PAS_IMPORTANCE;
         }
 

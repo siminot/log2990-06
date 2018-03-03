998ac086ae3728b5c42a28b1af72c921ac0e75bf
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-03-02 21:15:35 -0500

    Retrait de string magiques dans le code

--- client/src/app/crosswords/config-partie/config-partie.component.ts
+++ client/src/app/crosswords/config-partie/config-partie.component.ts
@@ -3,6 +3,10 @@ import { HttpeReqService } from "../httpRequest/http-request.service";
 import { ConfigurationPartie } from "./configurationPartie";
 
 export const REQUETE_INIT: string = "http://localhost:3000/grille/";
+const VISIBLE: string = "visible";
+const PAS_VISIBLE: string = "pasVisible";
+const CREER_OU_JOINDRE: string = "creerOuJoindre";
+const DIFFICULTE: string = "difficulte";
 
 @Component({
   selector: "app-config-partie",
@@ -11,39 +15,39 @@ export const REQUETE_INIT: string = "http://localhost:3000/grille/";
 })
 export class ConfigPartieComponent implements OnInit {
 
-  private requete: string;
+  private _requete: string;
   private lesOptions: ConfigurationPartie;
 
   public constructor(private serviceHTTP: HttpeReqService) {
-    this.requete = REQUETE_INIT;
+    this._requete = REQUETE_INIT;
     this.lesOptions = new ConfigurationPartie();
   }
 
   public ngOnInit(): void {
-    document.getElementById("difficulte").classList.add("pasVisible");
-    document.getElementById("creerOuJoindre").classList.add("pasVisible");
+    document.getElementById(DIFFICULTE).classList.add(VISIBLE);
+    document.getElementById(CREER_OU_JOINDRE).classList.add(PAS_VISIBLE);
   }
 
-  public get getRequete(): string {
-    return this.requete;
+  public get requete(): string {
+    return this._requete;
   }
 
   public modificationDeRequeteHTTP(): void {
-    this.serviceHTTP.modifierRequete(this.getRequete);
+    this.serviceHTTP.modifierRequete(this.requete);
   }
 
   public apparaitreSection(laSection: string): void {
-    document.getElementById(laSection).classList.remove("pasVisible");
-    document.getElementById(laSection).classList.add("visible");
+    document.getElementById(laSection).classList.remove(PAS_VISIBLE);
+    document.getElementById(laSection).classList.add(VISIBLE);
   }
 
   public disparaitreSection(laSection: string): void {
-    document.getElementById(laSection).classList.remove("visible");
-    document.getElementById(laSection).classList.add("pasVisible");
+    document.getElementById(laSection).classList.remove(VISIBLE);
+    document.getElementById(laSection).classList.add(PAS_VISIBLE);
   }
 
   public ajouterDifficulte(ajout: string): void {
-    if (ajout !== "") {
+    if (ajout.length > 0) {
       this.lesOptions.niveauDeDifficulte = ajout;
       this.miseAJourRequete();
       this.serviceHTTP.difficulte = this.lesOptions.niveauDeDifficulte;
@@ -51,7 +55,7 @@ export class ConfigPartieComponent implements OnInit {
   }
 
   private miseAJourRequete(): void {
-    this.requete = REQUETE_INIT + this.lesOptions.niveauDeDifficulte;
+    this._requete = REQUETE_INIT + this.lesOptions.niveauDeDifficulte;
   }
 
 }

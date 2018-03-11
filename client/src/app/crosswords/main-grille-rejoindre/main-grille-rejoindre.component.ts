8cb2aab84ba4026b8889d0a2926a13978072878f
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-03-11 03:05:24 -0400

    ML: correction erreur main-grille-rejoindre.component.ts

--- client/src/app/crosswords/main-grille-rejoindre/main-grille-rejoindre.component.ts
+++ client/src/app/crosswords/main-grille-rejoindre/main-grille-rejoindre.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-main-grille-rejoindre",
   templateUrl: "./main-grille-rejoindre.component.html",
-  styleUrls: ["../main-grille/main-grille.component.ts"]
+  styleUrls: ["../main-grille/main-grille.component.css"]
 })
 export class MainGrilleRejoindreComponent implements OnInit {
 

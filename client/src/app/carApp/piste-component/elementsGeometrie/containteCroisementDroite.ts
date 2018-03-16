64be72ea0975bc88fcb4b4ba011a5e101057b579
willylab <will.laberge16@gmail.com>
2018-03-16 10:29:45 -0400

    WL: modification des methodes contrainteCroisementdroite en statique

--- client/src/app/carApp/piste-component/elementsGeometrie/containteCroisementDroite.ts
+++ client/src/app/carApp/piste-component/elementsGeometrie/containteCroisementDroite.ts
@@ -3,44 +3,44 @@ import { Point } from "./Point";
 
 export class ContrainteCroitesementDroite {
 
-  public droitesSeCroisent(droite1: Droite, droite2: Droite): boolean {
+     public static droitesSeCroisent(droite1: Droite, droite2: Droite): boolean {
 
-    return this.boitesDroitesSeRecourbent(droite1.boite, droite2.boite)
-        && this.droiteCroiseOuToucheDroiteInfinie(droite1, droite2)
-        && this.droiteCroiseOuToucheDroiteInfinie(droite2, droite1);
-  }
+        return this.boitesDroitesSeRecourbent(droite1.boite, droite2.boite)
+                && this.droiteCroiseOuToucheDroiteInfinie(droite1, droite2)
+                && this.droiteCroiseOuToucheDroiteInfinie(droite2, droite1);
+    }
 
-  private boitesDroitesSeRecourbent(droite1: Droite, droite2: Droite): boolean {
-    return droite1.boite.start.x <= droite2.boite.end.x
-        && droite1.boite.end.x >= droite2.boite.start.x
-        && droite1.boite.start.z <= droite2.boite.end.z
-        && droite1.boite.end.z >= droite2.boite.start.z;
-  }
+     private static boitesDroitesSeRecourbent(droite1: Droite, droite2: Droite): boolean {
+        return droite1.boite.start.x <= droite2.boite.end.x
+                && droite1.boite.end.x >= droite2.boite.start.x
+                && droite1.boite.start.z <= droite2.boite.end.z
+                && droite1.boite.end.z >= droite2.boite.start.z;
+    }
 
-  private locationPointParRapportADroite(droite: Droite, point: Point): number {
-    const pointTemp: Point = new Point(point.x - droite.plusPetitX, point.y - droite.plusPetitY);
+     private static locationPointParRapportADroite(droite: Droite, point: Point): number {
+        const pointTemp: Point = new Point(point.x - droite.plusPetitX, point.y - droite.plusPetitY);
 
-    return droite.pointFinalDroiteCentree.produitVectoriel(pointTemp);
-  }
+        return droite.pointFinalDroiteCentree.produitVectoriel(pointTemp);
+    }
 
-  private pointEstSurDroite(droite: Droite, point: Point): boolean {
-    const DEGREE_ERREUR: number = 0.000001;
+     private static pointEstSurDroite(droite: Droite, point: Point): boolean {
+        const DEGREE_ERREUR: number = 0.000001;
 
-    return Math.abs(this.locationPointParRapportADroite(droite, point)) < DEGREE_ERREUR;
-  }
+        return Math.abs(this.locationPointParRapportADroite(droite, point)) < DEGREE_ERREUR;
+    }
 
-  private pointEstADroiteDeLaDroite(droite: Droite, point: Point): boolean {
-    return this.locationPointParRapportADroite(droite, point) < 0;
-  }
+     private static pointEstADroiteDeLaDroite(droite: Droite, point: Point): boolean {
+        return this.locationPointParRapportADroite(droite, point) < 0;
+    }
 
-  private droiteCroiseOuToucheDroiteInfinie(droite1: Droite, droite2: Droite): boolean {
-    const pointDebutdroite2: Point = new Point(droite2.start.x, droite2.start.z);
-    const pointFinDroite2: Point = new Point(droite2.end.x, droite2.end.z);
+     private static droiteCroiseOuToucheDroiteInfinie(droite1: Droite, droite2: Droite): boolean {
+        const pointDebutdroite2: Point = new Point(droite2.start.x, droite2.start.z);
+        const pointFinDroite2: Point = new Point(droite2.end.x, droite2.end.z);
 
-    return this.pointEstSurDroite(droite1, pointDebutdroite2)
-        || this.pointEstSurDroite(droite1, pointFinDroite2)
-        || (this.pointEstADroiteDeLaDroite(droite1, pointDebutdroite2)
-        !== this.pointEstADroiteDeLaDroite(droite1, pointFinDroite2));
-  }
+        return this.pointEstSurDroite(droite1, pointDebutdroite2)
+                || this.pointEstSurDroite(droite1, pointFinDroite2)
+                || (this.pointEstADroiteDeLaDroite(droite1, pointDebutdroite2)
+                !== this.pointEstADroiteDeLaDroite(droite1, pointFinDroite2));
+    }
 
 }

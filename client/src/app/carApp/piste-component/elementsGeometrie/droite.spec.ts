016139137c08ab67974bdf85d68fcd476e70dde6
willylab <will.laberge16@gmail.com>
2018-03-18 16:16:51 -0400

    WL: ajout du fichier droite.spec.ts et ajout de quelques test

---
+++ client/src/app/carApp/piste-component/elementsGeometrie/droite.spec.ts
@@ -0,0 +1,49 @@
+import { Droite } from "./Droite";
+import { Point } from "./Point";
+import { Vector3 } from "three";
+// import * as assert from "assert";
+
+// tslint:disable: no-magic-numbers
+describe("test de Droite: ", () => {
+
+    const droite: Droite = new Droite(new Point(6, 0), new Point(0, 5));
+    describe("Constructeur: ", () => {
+        it("le constructeur doit fonctionner", () => {
+            expect(droite).toBeTruthy();
+        });
+    });
+    describe("plusPetitX: ", () => {
+        it("retourne bien le plus petit x", () => {
+            expect(droite.plusPetitX).toEqual(0);
+        });
+    });
+    describe("plusGrandX: ", () => {
+        it("retourne bien le plus grand x", () => {
+            expect(droite.plusGrandX).toEqual(6);
+        });
+    });
+    describe("plusPetitY: ", () => {
+        it("retourne bien le plus petit y", () => {
+            expect(droite.plusPetitY).toEqual(0);
+        });
+    });
+    describe("plusGrandY: ", () => {
+        it("retourne bien le plus grand y", () => {
+            expect(droite.plusGrandY).toEqual(5);
+        });
+    });
+    describe("modifierDepart", () => {
+        it("le depart devrait etre modifie", () => {
+            const departInitial: Vector3 = droite.start;
+            droite.modifierDepart(new Point(5, 1));
+            expect(!departInitial).toEqual(droite.start);
+        });
+    });
+    describe("modifierArrivee", () => {
+        it("l'arrivee devrait etre modifiee", () => {
+            const arriveetInitiale: Vector3 = droite.end;
+            droite.modifierArrivee(new Point(1, 6));
+            expect(!arriveetInitiale).toEqual(droite.end);
+      });
+  });
+});

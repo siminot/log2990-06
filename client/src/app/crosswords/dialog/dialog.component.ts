fa8b0b4ed1a44d2d011886f007a2e724bc1a3add
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-03-19 16:49:11 -0400

    ML: options de fin de partie. 'Retour au menu principal' fonctionnel, mais pas 'Rejouer'.

--- client/src/app/crosswords/dialog/dialog.component.ts
+++ client/src/app/crosswords/dialog/dialog.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material";
+import { REQUETE_INIT } from "../config-partie/config-partie.component";
 
 @Component({
   selector: "app-dialog",
   templateUrl: "./dialog.component.html",
-  styleUrls: ["./dialog.component.css"]
+  styleUrls: ["./dialog.component.css"],
 })
 
 export class DialogComponent implements OnInit {
 
-  public constructor() { }
+  public constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   public ngOnInit(): void {
+  }
+
+  public rejouer(difficulte: string): void {
+    console.log("nouvelle partie", difficulte);
   }
 
 }

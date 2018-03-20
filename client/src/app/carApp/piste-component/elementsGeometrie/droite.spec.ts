8cc432d329f43666f8d14150158293835e1a3afc
willylab <will.laberge16@gmail.com>
2018-03-19 20:26:56 -0400

    WL: ajout de test pour droite

--- client/src/app/carApp/piste-component/elementsGeometrie/droite.spec.ts
+++ client/src/app/carApp/piste-component/elementsGeometrie/droite.spec.ts
@@ -32,18 +32,41 @@ describe("test de Droite: ", () => {
             expect(droite.plusGrandY).toEqual(5);
         });
     });
-    describe("modifierDepart", () => {
+    describe("modifierDepart: ", () => {
         it("le depart devrait etre modifie", () => {
             const departInitial: Vector3 = droite.start;
             droite.modifierDepart(new Point(5, 1));
-            assert(departInitial !== droite.start);
+            assert(!departInitial.equals(droite.start));
         });
     });
-    describe("modifierArrivee", () => {
+    describe("modifierArrivee: ", () => {
         it("l'arrivee devrait etre modifiee", () => {
             const arriveetInitiale: Vector3 = droite.end;
             droite.modifierArrivee(new Point(1, 6));
-            assert(arriveetInitiale !== droite.end);
-      });
-  });
+            assert(!arriveetInitiale.equals(droite.end));
+        });
+    });
+    describe("direction: ", () => {
+        it("la direction est la bonne", () => {
+            assert(droite.direction.equals(new Vector3(1 - 5, 0, 6 - 1)));
+        });
+    });
+
+    describe("angleAvecDroite: ", () => {
+        it("bon angle calcule", () => {
+            const droite1: Droite = new Droite(new Point(0, 0), new Point(1, 0));
+            const droite2: Droite = new Droite(new Point(0, 0), new Point(0, 1));
+            expect(droite1.angleAvecDroite(droite2)).toEqual(Math.PI / 2);
+        });
+    });
+    describe("boite: ", () => {
+        it("bonne boite", () => {
+            expect(droite.boite).toEqual(new Droite(new Point(1, 1), new Point(5, 6)));
+        });
+    });
+    describe("pointFinalDroiteCentree: ", () => {
+        it("", () => {
+            expect(droite.pointFinalDroiteCentree).toEqual(new Point(-4, 5));
+        });
+    });
 });

c9da19d2c9e05d55bed721b3001c74ea8dce9087
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-03-22 15:00:19 -0400

    Modification des constantes dans les tests de Point

--- client/src/app/carApp/elementsGeometrie/point.spec.ts
+++ client/src/app/carApp/elementsGeometrie/point.spec.ts
@@ -1,35 +1,34 @@
 import { Point } from "../elementsGeometrie/point";
 
-// tslint:disable: no-magic-numbers
 describe("Point", () => {
 
     const X: number = 5;
     const Y: number = -10;
-    const point: Point = new Point(X, Y);
-    const pointInverse: Point = new Point(Y, X);
+    const POINT: Point = new Point(X, Y);
+    const POINT_INVERSE: Point = new Point(Y, X);
     describe("Constructeur: ", () => {
         it("le constructeur doit fonctionner", () => {
-            expect(point).toBeTruthy();
-            expect(point.x).toEqual(X);
-            expect(point.y).toEqual(Y);
+            expect(POINT).toBeTruthy();
+            expect(POINT.x).toEqual(X);
+            expect(POINT.y).toEqual(Y);
         });
 
         it("le vecteur XY est correct", () => {
-            expect(point.vecteurPlanXZ.x).toEqual(X);
-            expect(point.vecteurPlanXZ.y).toEqual(0);
-            expect(point.vecteurPlanXZ.z).toEqual(Y);
+            expect(POINT.vecteurPlanXZ.x).toEqual(X);
+            expect(POINT.vecteurPlanXZ.y).toEqual(0);
+            expect(POINT.vecteurPlanXZ.z).toEqual(Y);
         });
 
         it("le vecteur XY est correct", () => {
-            expect(point.vecteurPlanXZ.x).toEqual(X);
-            expect(point.vecteurPlanXZ.y).toEqual(0);
-            expect(point.vecteurPlanXZ.z).toEqual(Y);
+            expect(POINT.vecteurPlanXZ.x).toEqual(X);
+            expect(POINT.vecteurPlanXZ.y).toEqual(0);
+            expect(POINT.vecteurPlanXZ.z).toEqual(Y);
         });
 
         it("L'egalite est detectee entre 2 points", () => {
-            expect(point.estEgalA(pointInverse)).not.toBeTruthy();
-            expect(point.estEgalA(new Point(X, Y))).toBeTruthy();
-            expect(point.estEgalA(point)).toBeTruthy();
+            expect(POINT.estEgalA(POINT_INVERSE)).not.toBeTruthy();
+            expect(POINT.estEgalA(new Point(X, Y))).toBeTruthy();
+            expect(POINT.estEgalA(POINT)).toBeTruthy();
         });
     });
 });

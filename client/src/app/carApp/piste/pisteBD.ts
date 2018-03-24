45048c7ff6abe7adc206ec033328df20ca615d72
willylab <will.laberge16@gmail.com>
2018-03-24 14:53:19 -0400

    WL: modification de la classe pisteBD pour une interface

--- client/src/app/carApp/piste/pisteBD.ts
+++ client/src/app/carApp/piste/pisteBD.ts
@@ -1,7 +1,7 @@
 import { Point } from "../elementsGeometrie/point";
 
-export class PisteBD {
-    public nom: string;
-    public description: string;
-    public points: Point[];
+export interface PisteBD {
+    nom: string;
+    description: string;
+    points: Point[];
 }

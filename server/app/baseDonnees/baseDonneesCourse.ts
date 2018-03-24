f16b193654168569ae004906e6a6beb3e714a53d
willylab <will.laberge16@gmail.com>
2018-03-24 14:52:52 -0400

    WL: ajout de ajouterPisteBidon

--- server/app/baseDonnees/baseDonneesCourse.ts
+++ server/app/baseDonnees/baseDonneesCourse.ts
@@ -1,23 +1,29 @@
-import { Mongoose, Model, Connection, Schema } from "mongoose";
+import { Mongoose, Model, Connection, Schema, Document } from "mongoose";
 import { RouteBaseDonneesCourse } from "./routeBaseDonneesCourse";
 import { Point } from "../../../client/src/app/carApp/elementsGeometrie/point";
+import { PisteBD } from "../../../client/src/app/carApp/piste/pisteBD";
+import { InterfacePointBaseDonnees } from "./interfacePointBD";
+
+const URL_BD: string = "mongodb://admin:admin@ds123129.mlab.com:23129/log2990";
 
 export class BaseDonneesCourse {
 
-    public readonly URL_BD: string = "mongodb://localhost:4200/baseDonnees";
-    private routeBaseDonneesCourse: RouteBaseDonneesCourse = new RouteBaseDonneesCourse();
-    private mongoose: Mongoose = new Mongoose();
+    private mongoose: Mongoose;
+    private schemaPiste: Schema;
+    private pistes: Model<Document>;
 
-    private schemaPiste: Schema = new Schema({
-        nom: String,
-        description: String,
-        points: [Point]
-    });
-
-    private piste: Model = this.mongoose.model("piste", this.schemaPiste);
+    constructor() {
+        this.mongoose = new Mongoose();
+        this.schemaPiste = new Schema({
+            nom: String,
+            description: String,
+            points: [{ x: Number, y: Number }],
+        });
+        this.pistes = this.mongoose.model("pistes", this.schemaPiste);
+    }
 
     public async seConnecter(): Promise<void> {
-        this.mongoose.connect("mongodb://" + this.routeBaseDonneesCourse.mainRoute)
+        this.mongoose.connect(URL_BD)
         .then(() => {
             console.log("connecte a la base de donnee");
         }).catch(() => {
@@ -29,9 +35,20 @@ export class BaseDonneesCourse {
         return this.mongoose.connection.readyState;
     }
 
+    public chargerModelPiste(): void {
+        this.mongoose.connection.model("pistes");
+    }
 
-
-
+    public async ajouterPisteBidon(): Promise<void> {
+        const longueur: number = 100;
+        const piste: Document =  new this.pistes ({
+            nom: "Piste 1",
+            description: "Parc au centre de la ville",
+            points: [   {x: -longueur, y: -longueur},
+                        {x: longueur, y: -longueur},
+                        {x: longueur, y: longueur},
+                        {x: -longueur , y: longueur}    ]
+        });
+        await this.pistes.create(piste);
+    }
 }
-
-

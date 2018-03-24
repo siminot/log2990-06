c044d5b13774e70e2b88a76a4492894e58c69f5a
willylab <will.laberge16@gmail.com>
2018-03-24 16:03:31 -0400

    WL: ajout des types et des bind de base de donnees course

--- server/app/inversify.config.ts
+++ server/app/inversify.config.ts
@@ -9,6 +9,9 @@ import { ServiceLexical } from "./serviceLexical/ServiceLexical";
 import { RouteGenGrille } from "./generateurGrille/routeGenGrille";
 import { GenerateurGrille } from "./generateurGrille/generateurGrille";
 
+import { RouteBaseDonneesCourse } from "./baseDonnees/routeBaseDonneesCourse";
+import { BaseDonneesCourse } from "./baseDonnees/baseDonneesCourse";
+
 const container: Container = new Container();
 
 container.bind(Types.Server).to(Server);
@@ -19,5 +22,8 @@ container.bind(Types.ServiceLexical).to(ServiceLexical);
 
 container.bind(Types.GenerateurGrille).to(GenerateurGrille);
 container.bind(Types.RouteGenGrille).to(RouteGenGrille);
+
+container.bind(Types.BaseDonneesCourse).to(BaseDonneesCourse);
+container.bind(Types.RouteBaseDonneesCourse).to(RouteBaseDonneesCourse);
 
 export { container };

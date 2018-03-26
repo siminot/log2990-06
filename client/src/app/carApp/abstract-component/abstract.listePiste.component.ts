a0ff6422de5a414d777a8b820b525c5e0d895ecd
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-03-26 16:17:59 -0400

    Modification de l'abstraction de ListePisteComponent pour integrer l'utilisation de la subscription

--- client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
+++ client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
@@ -1,12 +1,19 @@
 import { AfterViewInit } from "@angular/core";
-import { PisteBD } from "../piste/pisteBD";
+import { PisteBD } from "../piste/IPisteBD";
 import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
+import { Subscription } from "rxjs/Subscription";
 
 export abstract class AbstractListePisteComponent implements AfterViewInit {
 
     protected pistes: PisteBD[];
 
-    public constructor(protected gestionnaireBD: GestionnaireBDCourse) { }
+    public abonnementPistes: Subscription;
+
+    public constructor(protected gestionnaireBD: GestionnaireBDCourse) {
+        this.pistes = gestionnaireBD.pistes;
+        this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
+        .subscribe((pistes: PisteBD[]) => this.pistes = pistes);
+    }
 
     public ngAfterViewInit(): void {
       this.obtenirPistes();

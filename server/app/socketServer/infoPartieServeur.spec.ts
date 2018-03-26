e5be98944c7fa6c1107bbbe5c8bb1dcaa940d7ad
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-03-26 19:42:47 -0400

    ML: début test serveur

---
+++ server/app/socketServer/infoPartieServeur.spec.ts
@@ -0,0 +1,18 @@
+import * as assert from "assert";
+import { InfoPartieServeur } from "./infoPartieServeur";
+import { Socket } from "net";
+
+{
+    describe("infoPartieServeur", () => {
+        // let socket: SocketIO.Socket = {};
+        // let nom: string = "a";
+        // let difficulte: string = "difficile";
+        // let joueur: string = "b";
+
+        it("constructeur fonctionne", () => {
+            // let serveur: InfoPartieServeur = new InfoPartieServeur(nom, difficulte, joueur, socket);
+            // assert.notEqual(serveur, null || undefined);
+            assert(true);
+        });
+    });
+}

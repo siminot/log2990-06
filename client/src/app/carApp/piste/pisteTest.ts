c4c08829a94504d67553e345327eb4b9e5cbadfb
Sebastien Cadorette <cado.94@me.com>
2018-04-03 11:04:37 -0400

    SC : Ajustement du temps de tour des pistes de test.

--- client/src/app/carApp/piste/pisteTest.ts
+++ client/src/app/carApp/piste/pisteTest.ts
@@ -2,6 +2,7 @@ import { Point } from "../elementsGeometrie/point";
 import { PisteBD } from "./IPisteBD";
 
 const LONGUEUR: number = 100;
+
 export const PISTE_TEST: Point[] = [
     new Point(0, 0),
     new Point(-LONGUEUR, 0),
@@ -13,14 +14,18 @@ export const PISTE1: PisteBD = {
     _id: "1",
     nom: "Piste 1",
     description: "Parc au centre de la ville",
-    points: PISTE_TEST
+    points: PISTE_TEST,
+    infos: "Cette piste est un test parmi plusieurs. Enjoy the ride.",
+    tempsTours: [{ min: 0, sec: 0, milliSec: 0 }]
 };
 
 export const PISTE2: PisteBD = {
     _id: "2",
     nom: "Piste 2",
     description: "Champs de ble",
-    points: PISTE_TEST
+    points: PISTE_TEST,
+    infos: "Riders on the storm.",
+    tempsTours: [{ min: 0, sec: 0, milliSec: 0 }]
 };
 
 export const PISTES: PisteBD[] = [PISTE1, PISTE2];

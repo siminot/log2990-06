4eacab78e5b4242f06606449f4fd00cea177d27c
Sebastien Cadorette <cado.94@me.com>
2018-04-03 10:23:49 -0400

    SC : Ajustement de l'interface de la piste BD pour qu'elle prenne en compte les deux nouveaux éléments.

--- client/src/app/carApp/piste/IPisteBD.ts
+++ client/src/app/carApp/piste/IPisteBD.ts
@@ -1,8 +1,11 @@
 import { IDefinitionPoint } from "../../../../../common/communication/IDefinitionPoint";
+import { TempsTour } from "../../../../../common/communication/TempsTour";
 
 export interface PisteBD {
     _id: string;
     nom: string;
     description: string;
     points: IDefinitionPoint[];
+    infos: String;
+    tempsTours: [{ tempsTours: TempsTour }];
 }

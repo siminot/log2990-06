c0c7772cf3bdde0e34593c15a8db4f28fd28456f
Samuel Ferron <fersamw@gmail.com>
2018-04-03 09:53:04 -0400

    SF: Ajout de sons

--- client/src/app/carApp/son/SonDepart.ts
+++ client/src/app/carApp/son/SonDepart.ts
@@ -12,10 +12,14 @@ export class SonDepart extends SonAbstrait {
     }
 
     protected initialisationSon(): void {
-        this._audioLoader.load("./../../../assets/sons/JELAIPASENCORE",
+        this._audioLoader.load("./../../../assets/sons/raceStart.wav",
                                (buffer: THREE.AudioBuffer) => {
             this._audio.setBuffer(buffer);
         },                     null, null);
+    }
+
+    public get obtenirSon(): Audio {
+        return this._audio;
     }
 
     public jouerSon(): void {

9e8bf7784b9fb480aee32d24901e174370ab707b
Samuel Ferron <fersamw@gmail.com>
2018-04-04 22:24:15 -0400

    SF: Implementation de fonctions de bases pour le timer

--- client/src/app/carApp/timer/timer.service.ts
+++ client/src/app/carApp/timer/timer.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class TimerService {
 
-  constructor() { }
+    private tempsDebutCourse: number;
+    private tempsActuel: number;
+    private tempsDernierTour: number;
+
+    public constructor() {
+        this.tempsDebutCourse = 0;
+        this.tempsActuel = 0;
+    }
+
+    public debuterCourse(): void {
+        this.tempsDebutCourse = new Date().getTime();
+        this.partirTimer();
+    }
+
+    private partirTimer(): void {
+        setInterval(() => {
+            this.tempsActuel = new Date().getTime() - this.tempsDebutCourse;
+        },          1);
+    }
+
+    private actualisationTempsTour(): void {
+        this.tempsDernierTour = this.tempsDernierTour - this.tempsActuel;
+    }
+
+    public get obtenirTempsDernierTour(): number {
+        this.actualisationTempsTour();
+
+        return this.tempsDernierTour;
+    }
+
+    public get obtenirTempsActuel(): number {
+        return this.tempsActuel;
+    }
 
 }

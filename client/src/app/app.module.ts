b16d5d8aa5e4412053b2fb513864c766cdb38936
William La Berge <will.laberge16@gmail.com>
2018-04-06 09:19:20 -0400

    WL: ajout du component fin course

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -46,6 +46,7 @@ import { MatExpansionModule } from "@angular/material/expansion";
 import { MatDividerModule } from "@angular/material/divider";
 
 import { DecimalPipe } from "@angular/common";
+import { FinCourseComponent } from "./carApp/fin-course/fin-course.component";
 
 @NgModule({
     declarations: [
@@ -64,7 +65,8 @@ import { DecimalPipe } from "@angular/common";
         GrilleMultijoueurComponent,
         InfoPartieMultijoueurComponent,
         FinPartieSoloComponent,
-        MainGrilleMultiComponent
+        MainGrilleMultiComponent,
+        FinCourseComponent
     ],
     imports: [
         BrowserModule,
---
+++ client/src/app/carApp/fin-course/fin-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-fin-course",
+  templateUrl: "./fin-course.component.html",
+  styleUrls: ["./fin-course.component.css"]
+})
+export class FinCourseComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}

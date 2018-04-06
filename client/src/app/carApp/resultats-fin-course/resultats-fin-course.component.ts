6bb8c838ed8d652227da3bc98210120cb3e88b0a
Sebastien Cadorette <cado.94@me.com>
2018-04-06 10:41:48 -0400

    SC : Permet d'afficher les temps de la course sélectionnée par l'utilisateur.

--- client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.ts
+++ client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.ts
@@ -1,42 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
 import { PisteBD } from "../piste/IPisteBD";
 import { Subscription } from "rxjs/Subscription";
 
 @Component({
-  selector: "app-resultats-fin-course",
-  templateUrl: "./resultats-fin-course.component.html",
-  styleUrls: ["./resultats-fin-course.component.css"]
+    selector: "app-resultats-fin-course",
+    templateUrl: "./resultats-fin-course.component.html",
+    styleUrls: ["./resultats-fin-course.component.css"]
 })
 
 export class ResultatsFinCourseComponent implements OnInit {
-  public pistes: PisteBD[];
-  public abonnementPistes: Subscription;
+    public pistes: PisteBD[];
+    public abonnementPistes: Subscription;
 
-  public pisteCourante: PisteBD;
-  public abonnementPisteCourante: Subscription;
+    public pisteCourante: PisteBD;
+    public abonnementPisteCourante: Subscription;
 
-  public constructor(private gestionnaireBD: GestionnaireBDCourse) {
+    public unePiste: PisteBD = { _id: "1a", nom: "Une piste", description: "Une description",
+                                 points: null, infos: "Hello",
+                                 tempsTours: [{ nom: "Ken Block", min: 0, sec: 0, milliSec: 0}]};
 
-  }
-
-  public ngOnInit(): void {
-    this.abonnementPistesBD();
-    this.abonnementPisteCouranteBD();
-  }
-
-  private abonnementPistesBD(): void {
-    this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
-        .subscribe((pistes: PisteBD[]) => { this.pistes = pistes; this.abonnementPisteCouranteBD(); });
-  }
-
-  private abonnementPisteCouranteBD(): void {
-    console.log(this.pistes);
-    if (this.gestionnaireBD.pisteJeu === null) {
-        console.log("Assignation d'une piste de jeu au gestionnaire BD.");
-        this.gestionnaireBD.pisteJeu = this.pistes[0];
+    public constructor(private gestionnaireBD: GestionnaireBDCourse) {
     }
-    this.abonnementPisteCourante = this.gestionnaireBD.obtenirUnePiste(this.gestionnaireBD.pisteJeu._id)
-        .subscribe((piste: PisteBD) => { this.pisteCourante = piste; console.log(this.pisteCourante); });
-  }
+
+    public ngOnInit(): void {
+        this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
+            .subscribe((pistes: PisteBD[]) => this.pistes = pistes);
+
+        console.log(this.gestionnaireBD.pisteJeu);
+        if (this.gestionnaireBD.pisteJeu === null) {
+            this.gestionnaireBD.pisteJeu = this.unePiste;
+        }
+
+        this.pisteCourante = this.gestionnaireBD.pisteJeu;
+        console.log(this.pisteCourante);
+    }
+
 }

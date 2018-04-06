10dd30db0f496b0af062662f0f15e5963f726ff2
William La Berge <will.laberge16@gmail.com>
2018-04-06 10:54:04 -0400

    WL: ajustement  classe Resultat Joueur

--- client/src/app/carApp/fin-course/resultatJoueur.ts
+++ client/src/app/carApp/fin-course/resultatJoueur.ts
@@ -1,7 +1,13 @@
-// import {} from "../"
+import { TempsAffichage} from "../vue-tete-haute/vue-tete-haute/tempsAffichage";
 
 const NB_DE_TOURS: number = 3;
 
-class ResulatJoueur {
-    private tempsDesTours: TempsAffichage[];
+export class ResultatJoueur {
+
+    public rang: number;
+
+    private constructor(public tempsDesTours: TempsAffichage[],
+                        public tempsCourse: TempsAffichage[]) {
+        this.rang = null;
+    }
 }

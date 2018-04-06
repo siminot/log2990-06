ddf38ad82ea3fe1bbe0b26f89229c40b8c9fa4e4
Sebastien Cadorette <cado.94@me.com>
2018-04-06 08:58:06 -0400

    SC : Struggle avec les synchronisation de subscription.

--- client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.ts
+++ client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.ts
@@ -10,9 +10,11 @@ import { Subscription } from "rxjs/Subscription";
 })
 
 export class ResultatsFinCourseComponent implements OnInit {
+  public pistes: PisteBD[];
+  public abonnementPistes: Subscription;
 
-  private pistes: PisteBD[];
-  private abonnementPistes: Subscription;
+  public pisteCourante: PisteBD;
+  public abonnementPisteCourante: Subscription;
 
   public constructor(private gestionnaireBD: GestionnaireBDCourse) {
 
@@ -20,11 +22,21 @@ export class ResultatsFinCourseComponent implements OnInit {
 
   public ngOnInit(): void {
     this.abonnementPistesBD();
+    this.abonnementPisteCouranteBD();
   }
 
   private abonnementPistesBD(): void {
     this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
-        .subscribe((pistes: PisteBD[]) => { this.pistes = pistes; });
+        .subscribe((pistes: PisteBD[]) => { this.pistes = pistes; this.abonnementPisteCouranteBD(); });
   }
 
+  private abonnementPisteCouranteBD(): void {
+    console.log(this.pistes);
+    if (this.gestionnaireBD.pisteJeu === null) {
+        console.log("Assignation d'une piste de jeu au gestionnaire BD.");
+        this.gestionnaireBD.pisteJeu = this.pistes[0];
+    }
+    this.abonnementPisteCourante = this.gestionnaireBD.obtenirUnePiste(this.gestionnaireBD.pisteJeu._id)
+        .subscribe((piste: PisteBD) => { this.pisteCourante = piste; console.log(this.pisteCourante); });
+  }
 }

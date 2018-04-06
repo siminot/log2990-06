0cba2fe8031fd5fe0d454c71064e319266bccd5c
Marieve Lapierre <marieve.lapierre@gmail.com>
2018-04-06 16:26:34 -0400

    ML: sprite pour signal de départ positionné

--- client/src/app/carApp/serviceDeRendu/serviceDeRenduJeu.ts
+++ client/src/app/carApp/serviceDeRendu/serviceDeRenduJeu.ts
@@ -4,25 +4,24 @@ import { GestionnaireCamera } from "../camera/GestionnaireCamera";
 import { GestionnaireEcran } from "../ecran/gestionnaireEcran";
 import { ServiceDeRenduAbstrait } from "./servideDeRenduAbstrait";
 
-
 @Injectable()
 export class ServiceDeRenduJeu extends ServiceDeRenduAbstrait {
 
-    public courseEstCommencee: boolean;
+    // public courseEstCommencee: boolean;
 
-    public constructor(protected gestionnaireScene: GestionnaireScene,
+    public constructor(public gestionnaireScene: GestionnaireScene,
                        @Inject(GestionnaireEcran) gestionnaireEcran: GestionnaireEcran,
                        @Inject(GestionnaireCamera) gestionnaireCamera: GestionnaireCamera) {
         super(gestionnaireEcran, gestionnaireCamera, gestionnaireScene);
-        this.courseEstCommencee = false;
+        // this.courseEstCommencee = false;
     }
 
     // Rendu
 
     protected miseAJour(): void {
-        if (this.courseEstCommencee) {
+        // if (this.courseEstCommencee) {
         this.gestionnaireScene.miseAJour(Date.now() - this.tempsDerniereMiseAJour);
-        }
+        // }
         this.tempsDerniereMiseAJour = Date.now();
     }
 }

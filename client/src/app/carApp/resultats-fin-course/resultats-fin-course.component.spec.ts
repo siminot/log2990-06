becca119b8242c2c4ed34a423b8899483952c03b
Sebastien Cadorette <cado.94@me.com>
2018-04-08 14:00:48 -0400

    SC : Indentation du fichier avec les 4 espaces.

--- client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.spec.ts
+++ client/src/app/carApp/resultats-fin-course/resultats-fin-course.component.spec.ts
@@ -5,24 +5,24 @@ import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
 import { HttpClient, HttpHandler } from "@angular/common/http";
 
 describe("ResultatsFinCourseComponent", () => {
-  let component: ResultatsFinCourseComponent;
-  let fixture: ComponentFixture<ResultatsFinCourseComponent>;
+    let component: ResultatsFinCourseComponent;
+    let fixture: ComponentFixture<ResultatsFinCourseComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResultatsFinCourseComponent ],
-      providers: [GestionnaireBDCourse, HttpClient, HttpHandler]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ResultatsFinCourseComponent],
+            providers: [GestionnaireBDCourse, HttpClient, HttpHandler]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResultatsFinCourseComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ResultatsFinCourseComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });

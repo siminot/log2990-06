300db2cc0dc176844e50aeb258ed30b30accfe8d
Sebastien Cadorette <cado.94@me.com>
2018-04-08 22:41:56 -0400

    SC : Élaboration de tests encore pour la BD.

--- server/app/baseDonnees/routeBaseDonneesCourse.spec.ts
+++ server/app/baseDonnees/routeBaseDonneesCourse.spec.ts
@@ -5,25 +5,26 @@
 import * as assert from "assert";
 import * as WebRequest from "web-request";
 import { PisteBD } from "../../../client/src/app/carApp/piste/IPisteBD";
+
 // import { RouteBaseDonneesCourse } from "./routeBaseDonneesCourse";
 // import { ServiceWeb } from "../serviceweb";
 // import { PISTE1 } from "../../../client/src/app/carApp/piste/pisteTest";
 
 const URL_SERVICE: string = "http://localhost:3000/apipistes/";
-// const URL_AJOUTER: string = URL_SERVICE + "ajouter/";
+const URL_AJOUTER: string = URL_SERVICE + "ajouter/";
 // const URL_MODIFIER: string = URL_SERVICE + "modifier/";
 // const URL_SUPPRIMER: string = URL_SERVICE + "supprimer/";
 
 {
-    describe ("Tests sur les routes de la base de données.", () => {
-        describe ("Accéder aux routes (serveur doit rouler pour que ca fonctionne)", async () => {
-            describe ("Obtention de pistes.", () => {
-                it ("Doit retourner un tableau non-vide", async () => {
+    describe("Tests sur les routes de la base de données.", () => {
+        describe("Accéder aux routes (serveur doit rouler pour que ca fonctionne)", async () => {
+            describe("Obtention de pistes.", () => {
+                it("Doit retourner un tableau non-vide", async () => {
                     const PISTES: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
                     assert.notEqual(PISTES.length, 0);
                 });
 
-                it ("Reception de pistes.", async () => {
+                it("Reception de pistes.", async () => {
                     const DATA: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
                     assert.ok(DATA);
                 });

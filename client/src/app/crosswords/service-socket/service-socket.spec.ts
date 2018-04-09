5fe53b15cf39d8278994c52da73addb754c2dd84
Samuel Ferron <fersamw@gmail.com>
2018-04-08 21:50:37 -0400

    SF: Formatage des tests

--- client/src/app/crosswords/service-socket/service-socket.spec.ts
+++ client/src/app/crosswords/service-socket/service-socket.spec.ts
@@ -1,20 +1,20 @@
-import { TestBed, inject } from "@angular/core/testing";
-
 import { SocketService } from "./service-socket";
 import { Router } from "@angular/router";
+import { TestBed, inject } from "@angular/core/testing";
 
 describe("ServiceSocketService", () => {
-  // On veut utiliser let afin de ne pas l'initialser
-  // tslint:disable-next-line:prefer-const
-  let mockRouter: Router;
+    // On veut utiliser let afin de ne pas l'initialser
+    // tslint:disable-next-line:prefer-const
+    let mockRouter: Router;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SocketService, { provide: Router, useValue: mockRouter }]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [SocketService, { provide: Router, useValue: mockRouter }]
+        });
     });
-  });
 
-  it("should be created", inject([SocketService], (service: SocketService) => {
-    expect(service).toBeTruthy();
-  }));
+    it("should be created", inject([SocketService], (service: SocketService) => {
+        expect(service).toBeTruthy();
+    }));
+
 });

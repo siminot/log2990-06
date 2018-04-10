5206a20cec384ddd55d6dc05947051477a2b1c1b
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-04-10 10:59:00 -0400

    Extraction du Signal de  depart en une seule classe

--- client/src/app/carApp/scene/GestionnaireScene.ts
+++ client/src/app/carApp/scene/GestionnaireScene.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from "@angular/core";
-import { Scene, Sprite, Vector3, SpriteMaterial, Texture } from "three";
+import { Scene } from "three";
 import { IScene } from "./IScene";
 import { GestionnaireSkybox } from "../skybox/gestionnaireSkybox";
 import { GestionnaireVoitures } from "../voiture/gestionnaireVoitures";
@@ -10,12 +10,9 @@ import { PisteJeu } from "../piste/pisteJeu";
 import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
 import { TempsJournee } from "../skybox/tempsJournee";
 import { TEMPS_JOURNEE_INITIAL } from "../constants";
-import { SonDepart } from "../son/SonDepart";
 import { PISTE_TEST } from "../piste/pisteTest";
 import { Point } from "../elementsGeometrie/point";
-
-const TEMPS_ATTENTE: number = 10000;
-const TEMPS_SIGNAL_DEPART: number = 1000;
+import { SignalDepart } from "../signalDepart/signalDepart";
 
 // Touches clavier
 const CHANGER_DECOR: EvenementClavier = new EvenementClavier("t", TypeEvenementClavier.TOUCHE_RELEVEE);
@@ -28,7 +25,7 @@ export class GestionnaireScene implements IScene {
     private piste: PisteJeu;
     private tempsJournee: TempsJournee;
     private clavier: UtilisateurPeripherique;
-    public courseEstCommencee: boolean;
+    private signalDepart: SignalDepart;
 
     public get scene(): Scene {
         return this._scene;
@@ -41,7 +38,6 @@ export class GestionnaireScene implements IScene {
         this._scene = new Scene;
         this.clavier = new UtilisateurPeripherique(gestionnaireClavier);
         this.tempsJournee = TEMPS_JOURNEE_INITIAL;
-        this.courseEstCommencee = false;
         this.initialisationTouches();
         this.initialisationPiste(gestionnaireBDCourse.pointsJeu);
         this.creerScene();
@@ -62,7 +58,7 @@ export class GestionnaireScene implements IScene {
         this.clavier.ajouter(this.miseAJourTempsJournee.bind(this), CHANGER_HEURE_JOURNEE);
     }
 
-    public creerScene(): void {
+    private creerScene(): void {
         this.ajouterElements();
         this.avancerTemps();
         this.miseAJourTempsJournee();
@@ -78,61 +74,13 @@ export class GestionnaireScene implements IScene {
     }
 
     private signalerDepart(): void {
-        let sprite: Sprite = this.nouveauSignal("Préparez-vous au départ", "#0000ff");
-        this._scene.add(sprite);
-        setTimeout(() => {
-            this._scene.remove(sprite);
-            sprite = this.nouveauSignal("3", "#ff0000");
-            this._scene.add(sprite);
-            const sonDepart: SonDepart = new SonDepart();
-            this._scene.add(sonDepart.obtenirSon);
-            sonDepart.jouerSon();
-            setTimeout(() => {
-                this._scene.remove(sprite);
-                sprite = this.nouveauSignal("2", "#ff0000");
-                this._scene.add(sprite);
-                setTimeout(() => {
-                    this._scene.remove(sprite);
-                    sprite = this.nouveauSignal("1", "#ff0000");
-                    this._scene.add(sprite);
-                    setTimeout(() => {
-                        this._scene.remove(sprite);
-                        sprite = this.nouveauSignal("GO", "#00ff00");
-                        this._scene.add(sprite);
-                        this.courseEstCommencee = true;
-                        setTimeout(() => {
-                            this._scene.remove(sprite);
-                        },TEMPS_SIGNAL_DEPART);
-                    }, TEMPS_SIGNAL_DEPART);
-                },  TEMPS_SIGNAL_DEPART);
-            },  TEMPS_SIGNAL_DEPART);
-        },  TEMPS_ATTENTE);
-    }
-
-    private nouveauSignal(texte: string, couleur: string): Sprite {
-        const canvas: HTMLCanvasElement = document.createElement("canvas");
-        const size: number = 256; // CHANGED
-        canvas.width = size;
-        canvas.height = size;
-        const context: CanvasRenderingContext2D = canvas.getContext("2d");
-        context.fillStyle = couleur; // CHANGED
-        context.textAlign = "center";
-        context.font = "24px Arial";
-        context.fillText(texte, size / 2, size / 2);
-
-        const spriteMap: Texture = new Texture(canvas);
-        spriteMap.needsUpdate = true;
-        const spriteMaterial: SpriteMaterial = new SpriteMaterial({ map: spriteMap });
-        const sprite: Sprite = new Sprite(spriteMaterial);
-        sprite.scale.set(10, 10, 1);
-        const position: Vector3 = this.piste.zoneDeDepart;
-        sprite.position.set(position.x, 3, position.z);
-
-        return sprite;
+        this.signalDepart = new SignalDepart(this.piste.zoneDeDepart);
+        this._scene.add(this.signalDepart);
+        this.signalDepart.demarrer();
     }
 
     public miseAJour(tempsDepuisDerniereTrame: number): void {
-        if (this.courseEstCommencee) {
+        if (this.signalDepart.estTermine) {
             this.gestionnaireVoiture.miseAJourVoitures(tempsDepuisDerniereTrame);
         }
     }
---
+++ client/src/app/carApp/signalDepart/signalDepart.ts
@@ -0,0 +1,77 @@
+import { Sprite, Vector3, SpriteMaterial, Texture } from "three";
+import { SonDepart } from "../son/SonDepart";
+
+const ROUGE: string = "#ff0000";
+const VERT: string = "#00ff00";
+const BLEU: string = "#0000ff";
+
+const TEMPS_SIGNAL_DEPART: number = 1000;
+const TEMPS_MAXIMAL: number = 6;
+const DEBUT_COMPTEUR: number = 3;
+
+const MESSAGE_PREPARATION: string = "Préparez-vous au départ";
+const SIGNAL_DEPART: string = "GO";
+
+export class SignalDepart extends Sprite {
+
+    private compteur: number;
+    private readonly zoneDepart: Vector3;
+
+    public constructor(zoneDepart: Vector3) {
+        super();
+        this.zoneDepart = zoneDepart;
+        this.compteur = TEMPS_MAXIMAL;
+    }
+
+    public demarrer(): void {
+        if (this.compteur >= 0) {
+            this.passerAuProchainMessage();
+            setTimeout(() => {
+                const sonDepart: SonDepart = new SonDepart();
+                sonDepart.jouerSon();
+                this.demarrer();
+            },         TEMPS_SIGNAL_DEPART);
+        } else {
+            this.retirerMessage();
+        }
+    }
+
+    private passerAuProchainMessage(): void {
+        if (this.compteur > DEBUT_COMPTEUR) {
+            this.compteur--;
+            this.nouveauSignal(MESSAGE_PREPARATION, BLEU);
+        } else if (this.compteur > 0) {
+            this.nouveauSignal((this.compteur--).toString(), ROUGE);
+        } else {
+            this.compteur--;
+            this.nouveauSignal(SIGNAL_DEPART, VERT);
+        }
+    }
+
+    private nouveauSignal(texte: string, couleur: string): void {
+        const canvas: HTMLCanvasElement = document.createElement("canvas");
+        const size: number = 256; // CHANGED
+        canvas.width = size;
+        canvas.height = size;
+        const context: CanvasRenderingContext2D = canvas.getContext("2d");
+        context.fillStyle = couleur; // CHANGED
+        context.textAlign = "center";
+        context.font = "24px Arial";
+        context.fillText(texte, size / 2, size / 2);
+
+        const spriteMap: Texture = new Texture(canvas);
+        spriteMap.needsUpdate = true;
+        const spriteMaterial: SpriteMaterial = new SpriteMaterial({ map: spriteMap });
+        this.material.setValues(spriteMaterial);
+        this.scale.set(10, 10, 1);
+        this.position.set(this.zoneDepart.x, 3, this.zoneDepart.z);
+    }
+
+    private retirerMessage(): void {
+        this.nouveauSignal("", VERT);
+    }
+
+    public get estTermine(): boolean {
+        return this.compteur < 0;
+    }
+}

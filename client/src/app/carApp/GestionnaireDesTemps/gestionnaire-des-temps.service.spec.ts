e080a968a8ade7b06a4d186074a8a5eea5b86c68
Samuel Ferron <fersamw@gmail.com>
2018-04-11 22:18:12 -0400

    SF: Structure de la classe + test unitaires
    Tests d'erreurs avec utilisation d'un faux timer

--- client/src/app/carApp/GestionnaireDesTemps/gestionnaire-des-temps.service.spec.ts
+++ client/src/app/carApp/GestionnaireDesTemps/gestionnaire-des-temps.service.spec.ts
@@ -1,15 +1,83 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import { GestionnaireDesTempsService } from "./gestionnaire-des-temps.service";
+import { TimerService } from "../timer/timer.service";
+import { InvalidArgumentError } from "../../exceptions/invalidArgumentError";
+
+const TEMPSTEST: number = 12345;
+
+class MockTimer {
+
+    private tempsTest: number = TEMPSTEST;
+
+    public get nouveauTour(): number { return this.tempsTest; }
+    public get obtenirTempsActuel(): number { return this.tempsTest; }
+    public get obtenirTempsTourJoueur(): number { return this.tempsTest; }
+
+}
 
 describe("GestionnaireDesTempsService", () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [GestionnaireDesTempsService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [GestionnaireDesTempsService,
+                        { provide: TimerService, useClass: MockTimer }]
+        });
     });
-  });
 
-  it("should be created", inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
-    expect(service).toBeTruthy();
-  }));
+    it("Devrait bien construire", inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    describe("Actualisation du temps de course course", () => {
+
+        it("Devrait changer le temps de la course pour le premier AI",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            //
+        }));
+
+        it("Devrait changer le temps de la course pour le deuxieme AI",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            //
+        }));
+
+        it("Devrait changer le temps de la course pour le troisieme AI",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            //
+        }));
+
+        it("Devrait lancer une erreur",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            expect( () => { service.AIXCourseComplete(3); }).toThrow(new InvalidArgumentError());
+        }));
+
+        it("Devrait lancer une erreur",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            expect( () => { service.AIXCourseComplete(-10); }).toThrow(new InvalidArgumentError());
+        }));
+
+    });
+
+    describe("Ajout de temps de tour", () => {
+
+        it("Devrait changer le temps de la course pour le premier AI",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+               //
+        }));
+
+        it("Devrait lancer une erreur",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            expect( () => { service.AIxTourComplete(3); }).toThrow(new InvalidArgumentError());
+        }));
+
+        it("Devrait lancer une erreur",
+           inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+            expect( () => { service.AIxTourComplete(-10); }).toThrow(new InvalidArgumentError());
+        }));
+
+    });
+
+    // Prototype de test
+    // it("", inject([GestionnaireDesTempsService], (service: GestionnaireDesTempsService) => {
+
+    // }));
+
 });
--- client/src/app/carApp/GestionnaireDesTemps/gestionnaire-des-temps.service.ts
+++ client/src/app/carApp/GestionnaireDesTemps/gestionnaire-des-temps.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from "@angular/core";
+import { TimerService } from "../timer/timer.service";
+import { TempsAffichage } from "../vue-tete-haute/vue-tete-haute/tempsAffichage";
+import { InvalidArgumentError } from "../../exceptions/invalidArgumentError";
+
+const NBR_JOUEURS: number = 4;
 
 @Injectable()
 export class GestionnaireDesTempsService {
 
-  public constructor() { }
+    private tempsJoueur: TempsAffichage;
+    private tempsAIs: Array<TempsAffichage>;
 
+    public constructor(private timer: TimerService) {
+        this.initialisation();
+    }
+
+    private initialisation(): void {
+        this.tempsJoueur = new TempsAffichage();
+        this.tempsAIs = new Array<TempsAffichage>(NBR_JOUEURS - 1);
+        for (let i: number = 0; i < NBR_JOUEURS - 1; i++) {
+            this.tempsAIs[i] = new TempsAffichage();
+        }
+    }
+
+    public set actualiserTempsJoueur(temps: TempsAffichage) {
+        this.tempsJoueur = temps;
+    }
+
+    public AIxTourComplete(noJoueur: number): void {
+        this.verifierIndex(noJoueur);
+        this.timer.nouveauTour(noJoueur); // juste pour pas avoir de probleme de compil...
+    }
+
+    public AIXCourseComplete(noJoueur: number): void {
+        this.verifierIndex(noJoueur);
+        this.tempsAIs[noJoueur].tempsAffichable = this.timer.obtenirTempsActuel;
+    }
+
+    private verifierIndex(noJoueur: number): void {
+        if (noJoueur < 0 || noJoueur >= NBR_JOUEURS - 1) {
+            throw new InvalidArgumentError();
+        }
+    }
+
+    public get obtenirTempsDesJoueurs(): Array<TempsAffichage> {
+        const tempsFinCourse: Array<TempsAffichage> = new Array<TempsAffichage>();
+        tempsFinCourse.push(this.tempsJoueur);
+        for (const tempsAi of this.tempsAIs) {
+            tempsFinCourse.push(tempsAi);
+        }
+
+        return tempsFinCourse;
+    }
 }

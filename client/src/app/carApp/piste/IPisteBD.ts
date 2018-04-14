e3948fd36d87f7c967075a56c787f22e0166ce5d
Sebastien Cadorette <cado.94@me.com>
2018-04-14 18:23:24 -0400

    SC : Ajout d'une interface pour le temps.

--- client/src/app/carApp/piste/IPisteBD.ts
+++ client/src/app/carApp/piste/IPisteBD.ts
@@ -1,11 +1,18 @@
 import { IDefinitionPoint } from "../../../../../common/communication/IDefinitionPoint";
 
+export interface ITemps {
+    nom: string;
+    min: number;
+    sec: number;
+    milliSec: number;
+}
+
 export interface PisteBD {
     _id: string;
     nom: string;
     description: string;
     type: string;
     points: IDefinitionPoint[];
-    temps: [{ nom: string, min: number, sec: number, milliSec: number }];
+    temps: ITemps[];
     nbFoisJoue: number;
 }

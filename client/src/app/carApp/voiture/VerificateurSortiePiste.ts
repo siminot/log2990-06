f426ffb5584d3fca8a8d336fbbce5db5b301d50c
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-04-13 21:59:25 -0400

    Ajout de 4 raycaster pour detecter la sortie de piste

--- client/src/app/carApp/voiture/VerificateurSortiePiste.ts
+++ client/src/app/carApp/voiture/VerificateurSortiePiste.ts
@@ -1,37 +1,45 @@
-import { Object3D, Mesh, SphereGeometry, MeshBasicMaterial, Raycaster, Vector3, Intersection } from "three";
+import { Object3D, Raycaster, Vector3, Intersection } from "three";
 import { NOM_PISTE_JEU } from "../constants";
 
 const BAS: Vector3 = new Vector3(0, -1, 0);
 const LARGEUR: number = 0.5;
-const PROFONDEUR: number = -1.65;
+const PROFONDEUR: number = -1.55;
 const HAUTEUR: number = 0.5;
 const PLAN_RAPPROCHE: number = 5;
 const PLAN_ELOIGNE: number = -5;
+
+const POSITIONS_RAYONS: Vector3[] = [
+    new Vector3( LARGEUR, HAUTEUR,  PROFONDEUR),
+    new Vector3( LARGEUR, HAUTEUR, -PROFONDEUR),
+    new Vector3(-LARGEUR, HAUTEUR,  PROFONDEUR),
+    new Vector3(-LARGEUR, HAUTEUR, -PROFONDEUR),
+];
 
 export class VerificateurSortiePiste extends Object3D {
 
     public constructor() {
         super();
-
-        const mesh1: Mesh = new Mesh(new SphereGeometry(1), new MeshBasicMaterial( {color: 0xFFFFFF}));
-        // mesh1.setPosition = position = BAS;
-        this.add(mesh1);
     }
 
     public estSurPiste(): boolean {
         for (const rayon of this.rayons) {
-            if (this.contientLaPiste(rayon.intersectObject(this.scene, true))) {
-                return true;
+            if (!this.contientLaPiste(rayon.intersectObject(this.scene, true))) {
+                return false;
             }
         }
 
-        return false;
+        return true;
     }
 
     private get rayons(): Raycaster[] {
-        const position: Vector3 = new Vector3(this.voiture.position.x, HAUTEUR, this.voiture.position.z);
+        const rayons: Raycaster[] = [];
+        const positionVoiture: Vector3 = new Vector3(this.voiture.position.x, 0, this.voiture.position.z);
 
-        return [new Raycaster(position, BAS, PLAN_ELOIGNE, PLAN_RAPPROCHE)];
+        for (const position of POSITIONS_RAYONS) {
+            rayons.push(new Raycaster(positionVoiture.clone().add(position), BAS, PLAN_ELOIGNE, PLAN_RAPPROCHE));
+        }
+
+        return rayons;
     }
 
     private contientLaPiste(elements: Intersection[]): boolean {

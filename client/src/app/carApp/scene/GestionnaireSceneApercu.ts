98d137409f5476cce142c99891f2adc372ef10a8
Simon Turcotte <simon-2.turcotte@polymtl.ca>
2018-04-16 07:40:05 -0400

    Ajout d'un fond d'écran noir à l'aperçu pour tester son affichage

--- client/src/app/carApp/scene/GestionnaireSceneApercu.ts
+++ client/src/app/carApp/scene/GestionnaireSceneApercu.ts
@@ -1,25 +1,38 @@
 import { Injectable, Input } from "@angular/core";
-import { Scene, Vector3 } from "three";
+import { Scene, MeshBasicMaterial, PlaneGeometry, Mesh, DoubleSide } from "three";
 import { IScene } from "./IScene";
 import { PisteJeu } from "../piste/pisteJeu";
 import { PISTE_TEST } from "../piste/pisteTest";
 import { Point } from "../elementsGeometrie/point";
+import { PI_OVER_2 } from "../constants";
+import { PROFONDEUR_SCENE } from "./GestionnaireScenePiste";
+
+const COULEUR_FOND: number = 0xFFFFFF;
 
 @Injectable()
-export class GestionnaireSceneApercu implements IScene {
+export class GestionnaireSceneApercu extends Scene implements IScene {
 
-    private _scene: Scene;
     private piste: PisteJeu;
     @Input() private points: Point[];
 
     public get scene(): Scene {
-        return this._scene;
+        return this;
     }
 
     public constructor() {
-        this._scene = new Scene;
+        super();
         this.initialisationPiste();
-        this._scene.add(this.piste);
+        this.add(this.piste);
+        this.ajouterCouleurDeFond(); // POUR TESTER
+    }
+
+    private ajouterCouleurDeFond(): void {
+        const DIMENSIONS: number = 50000;
+        const MATERIEL: MeshBasicMaterial = new MeshBasicMaterial({ color: COULEUR_FOND, side: DoubleSide });
+        const geometrie: PlaneGeometry = new PlaneGeometry(DIMENSIONS, DIMENSIONS);
+        geometrie.rotateX(PI_OVER_2);
+        geometrie.translate(0, PROFONDEUR_SCENE, 0);
+        this.add(new Mesh(geometrie, MATERIEL));
     }
 
     private initialisationPiste(): void {
@@ -30,13 +43,5 @@ export class GestionnaireSceneApercu implements IScene {
             this.piste = new PisteJeu();
             this.piste.importer(PISTE_TEST);
         }
-    }
-
-    public get obtenirPoints(): Point[] {
-        return this.piste.exporter();
-    }
-
-    public get obtenirZoneDepart(): Vector3 {
-        return this.piste.zoneDeDepart;
     }
 }

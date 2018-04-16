a0b8013013d1b699ed81c8ef826ff4af50bba05e
Sebastien Cadorette <cado.94@me.com>
2018-04-16 18:26:15 -0400

    SC : Création de la directive pour les chiffres.

--- client/src/app/carApp/directive-est-un-chiffre/est-un-chiffre.directive.ts
+++ client/src/app/carApp/directive-est-un-chiffre/est-un-chiffre.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from "@angular/core";
+import { Directive, Input, HostListener } from "@angular/core";
+import { CHIFFRE_UN, CHIFFRE_NEUF } from "../constants";
 
 @Directive({
   selector: "[appEstUnChiffre]"
 })
 export class EstUnChiffreDirective {
 
-  public constructor() { }
+    @Input()
+    public appEstUnChiffre: boolean;
 
+    public constructor() { }
+
+    @HostListener("keydown", ["$event"])
+    public onKeyDown(event: KeyboardEvent): void {
+      if (this.appEstUnChiffre && !this.estUnChifre(event)) {
+        event.preventDefault();
+      }
+    }
+
+    private estUnChifre(event: KeyboardEvent): boolean {
+      return event.key >= CHIFFRE_UN && event.key <= CHIFFRE_NEUF;
+    }
 }

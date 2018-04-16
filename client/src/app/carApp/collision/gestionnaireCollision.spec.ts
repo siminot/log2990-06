8528c516194044f85367fc58f6377f0da4eea00a
J-F <jffontaine_@hotmail.com>
2018-04-16 13:08:37 -0400

    fichier test fonctionnel.

--- client/src/app/carApp/collision/gestionnaireCollision.spec.ts
+++ client/src/app/carApp/collision/gestionnaireCollision.spec.ts
@@ -1,24 +1,31 @@
-/*
-// import { TestBed, ComponentFixture} from "@angular/core/testing";
+import { TestBed, ComponentFixture} from "@angular/core/testing";
 import {GestionnaireCollision } from "./gestionnaireCollisions";
+import { Voiture } from "../voiture/voiture";
 
+const NOMBRE_AUTO: number = 4;
 describe("Gestionnaire de collisions", () => {
     let gestionnaireCollision: GestionnaireCollision;
+    // let fixture: ComponentFixture;
 
-    // beforeEach(async() => {
-    //     TestBed.configureTestingModule({
-    //     declarations: [GestionnaireCollision ],
-    //     // providers: []
-    //     }).compileComponents();
-    // });
+    beforeEach(async() => {
+        TestBed.configureTestingModule({
+        declarations: [GestionnaireCollision ],
+        // providers: []
+        }).compileComponents();
+    });
     beforeEach( () => {
         // fixture = TestBed.createComponent(GestionnaireCollision);
         // gestionnaireCollision = fixture.componentInstance;
-        gestionnaireCollision = new GestionnaireCollision();
+        const voitureAI: Array<Voiture> = new Array<Voiture>();
+        const voitureJoueur: Voiture = new Voiture();
+        for (let i: number = 0; i < NOMBRE_AUTO ; i++) {
+            voitureAI.push(new Voiture());
+        }
+        gestionnaireCollision = new GestionnaireCollision(voitureJoueur, voitureAI);
+
     });
 
     it("Construction", () => {
         expect(gestionnaireCollision).toBeTruthy();
     });
 });
-*/

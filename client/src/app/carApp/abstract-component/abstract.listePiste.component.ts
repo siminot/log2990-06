e83a4e39f3bee0147a9103380e24ec27ce7f7252
Sebastien Cadorette <cado.94@me.com>
2018-04-16 10:43:05 -0400

    SC : Oublié de changer de branche encore ... Ajout d'un abonnement & d'un désabonnement aux pistes.

--- client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
+++ client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
@@ -1,10 +1,12 @@
-import { OnInit } from "@angular/core";
+import { OnInit, OnDestroy } from "@angular/core";
 import { PisteBD } from "../piste/IPisteBD";
 import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
+import { Subscription } from "rxjs/Subscription";
 
-export abstract class AbstractListePisteComponent implements OnInit {
+export abstract class AbstractListePisteComponent implements OnInit, OnDestroy {
 
     protected pistes: PisteBD[];
+    private abonnementPistes: Subscription;
 
     public constructor(protected gestionnaireBD: GestionnaireBDCourse) {}
 
@@ -13,7 +15,15 @@ export abstract class AbstractListePisteComponent implements OnInit {
     }
 
     public obtenirPistes(): void {
-        this.gestionnaireBD.obtenirPistes()
+        this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
             .subscribe((pistes: PisteBD[]) => this.pistes = pistes);
+    }
+
+    public desinscriptionAuxPistes(): void {
+        this.abonnementPistes.unsubscribe();
+    }
+
+    public ngOnDestroy(): void {
+        this.desinscriptionAuxPistes();
     }
 }

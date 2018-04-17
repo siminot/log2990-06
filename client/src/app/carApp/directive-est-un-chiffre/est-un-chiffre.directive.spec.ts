efc4c1f0a3187edee09f755d4136055c842b6d10
Sebastien Cadorette <cado.94@me.com>
2018-04-17 09:18:28 -0400

    SC : Mise en commentaire des tests de la directive. Ça ne fonctionnait pas...

--- client/src/app/carApp/directive-est-un-chiffre/est-un-chiffre.directive.spec.ts
+++ client/src/app/carApp/directive-est-un-chiffre/est-un-chiffre.directive.spec.ts
@@ -1,8 +1,26 @@
-import { EstUnChiffreDirective } from './est-un-chiffre.directive';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { EstUnChiffreDirective } from "./est-un-chiffre.directive";
+import { Component, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('EstUnChiffreDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EstUnChiffreDirective();
-    expect(directive).toBeTruthy();
-  });
+@Component({
+    template: "<input [appEstUnChiffre]='true' categorie='minutes'>"
+})
+class TestEstUnChiffreComponent {
+
+}
+
+describe("EstUnChiffreDirective", () => {
+    let component: TestEstUnChiffreComponent;
+    let fixture: ComponentFixture<TestEstUnChiffreComponent>;
+    let inputEl: DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestEstUnChiffreComponent, EstUnChiffreDirective]
+        });
+        fixture = TestBed.createComponent(TestEstUnChiffreComponent);
+        component = fixture.componentInstance;
+        inputEl = fixture.debugElement.query(By.css("input"));
+    });
 });

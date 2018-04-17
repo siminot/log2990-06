168f3f7aab18f1c4faa55a9d53ac904c5aaca881
William La Berge <will.laberge16@gmail.com>
2018-04-17 10:02:33 -0400

    WL: modification des attributs de resultats joueurs

--- client/src/app/carApp/fin-course/resultatJoueur.ts
+++ client/src/app/carApp/fin-course/resultatJoueur.ts
@@ -1,13 +1,17 @@
 import { TempsAffichage} from "../vue-tete-haute/vue-tete-haute/tempsAffichage";
+import { TempsJoueur } from "../GestionnaireDesTemps/tempsJoueur";
 
 export class ResultatJoueur {
 
     public position: number;
+    public nom: string;
+    public tempsCourse: TempsAffichage;
+    public tempsTours: Array<TempsAffichage>;
 
-    public constructor( public nom: string,
-                        public joueurEstHumain: boolean,
-                        public tempsDesTours: TempsAffichage[],
-                        public tempsCourse: TempsAffichage) {
+    public constructor(nom: string, tempsJoueur: TempsJoueur) {
         this.position = null;
+        this.nom = nom;
+        this.tempsCourse = tempsJoueur.tempsCourse;
+        this.tempsTours = this.tempsTours;
     }
 }

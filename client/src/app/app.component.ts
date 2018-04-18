8e048255aabb668c53aff3ca65f1b5ba53cc07ae
SebastienCadorette <cado.94@me.com>
2018-04-18 14:25:40 -0400

    SC : Ajout d'une image en page principale.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from "@angular/core";
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"]
 })
-export class AppComponent { }
+export class AppComponent {
+
+    public retirerBackgroundImage(): void {
+        document.getElementById("bg").id = "nobg";
+    }
+
+    public remettreImageBackground(): void {
+        document.getElementById("nobg").id = "bg";
+    }
+ }

05be20d4f7762e4e5405b32cf4104ee2d64f22eb
SebastienCadorette <cado.94@me.com>
2018-04-18 19:34:12 -0400

    SC : Ajout d'une fonction pour classer les temps.

--- client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
+++ client/src/app/carApp/abstract-component/abstract.listePiste.component.ts
@@ -2,6 +2,8 @@ import { OnInit, OnDestroy } from "@angular/core";
 import { PisteBD } from "../piste/IPisteBD";
 import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
 import { Subscription } from "rxjs/Subscription";
+import { ITempsBD } from "../piste/ITempsBD";
+import * as CONST from "../constants";
 
 export abstract class AbstractListePisteComponent implements OnInit, OnDestroy {
 
@@ -16,7 +18,10 @@ export abstract class AbstractListePisteComponent implements OnInit, OnDestroy {
 
     public obtenirPistes(): void {
         this.abonnementPistes = this.gestionnaireBD.obtenirPistes()
-            .subscribe((pistes: PisteBD[]) => this.pistes = pistes);
+            .subscribe((pistes: PisteBD[]) => {
+                this.pistes = pistes;
+                this.classerTemps();
+            });
     }
 
     public desinscriptionAuxPistes(): void {
@@ -25,5 +30,18 @@ export abstract class AbstractListePisteComponent implements OnInit, OnDestroy {
 
     public ngOnDestroy(): void {
         this.desinscriptionAuxPistes();
+    }
+
+    protected classerTemps(): void {
+        for (const piste of this.pistes) {
+            piste.temps.sort((a: ITempsBD, b: ITempsBD) => {
+                const tmpA: number = a.milliSec + a.sec * CONST.SECONDS_TO_MILLISECS +
+                    a.min * CONST.MIN_TO_MILLISECS;
+                const tmpB: number = b.milliSec + b.sec * CONST.SECONDS_TO_MILLISECS +
+                    b.min * CONST.MIN_TO_MILLISECS;
+
+                return tmpA > tmpB ? 1 : -1;
+            });
+        }
     }
 }

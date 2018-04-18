c8388f091f4a28baa0ee86edf507e60d53dac088
SebastienCadorette <cado.94@me.com>
2018-04-18 18:34:23 -0400

    SC : Ajout d'un host listener sur les touches pressées.

--- client/src/app/carApp/directive-nb-caract-max/nb-caract-max.directive.ts
+++ client/src/app/carApp/directive-nb-caract-max/nb-caract-max.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from "@angular/core";
+import { Directive, Input, HostListener } from "@angular/core";
+import * as CONST from "../constants";
 
 @Directive({
   selector: "[appNbCaractMax]"
 })
 export class NbCaractMaxDirective {
 
+  @Input()
+  private appNbCaractMax: string;
+
   public constructor() { }
+
+  @HostListener("keydown", ["$event"])
+  public onKeyDown(event: KeyboardEvent): void {
+    
+  }
 
 }

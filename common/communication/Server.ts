0bd15ff718e09077bb842feef18bd33c168feedf
Simon Turcotte <turcotte.simon@hotmail.com>
2019-03-03 19:09:40 +0000

    Modification de l'url de la BD

--- common/communication/Server.ts
+++ common/communication/Server.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL: string = "http://localhost:3000/";
-export const MONGO_DB_URL: string = "mongodb://admin:admin@ds123129.mlab.com:23129/log2990";
+export const SERVER_URL: string = "http://siminot.step.polymtl.ca:3000/";
+export const MONGO_DB_URL: string = "mongodb://admin:12345@127.0.0.1:27017";

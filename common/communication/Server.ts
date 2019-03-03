7f7b32ec580d1d6819e177337ec4b27a939d708b
siminot <turcotte.simon@hotmail.com>
2019-03-03 15:13:09 -0500

    Modification de l'url du serveur

--- common/communication/Server.ts
+++ common/communication/Server.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL: string = "http://siminot.step.polymtl.ca:3000/";
+export const SERVER_URL: string = "http://siminot.step.polymtl.ca/server/";
 export const MONGO_DB_URL: string = "mongodb://admin:12345@127.0.0.1:27017";
